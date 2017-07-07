00cf9559435687cf10f157731a1589a2e0f8b956
Akita Noek <anoek@online-go.com>
2017-07-07 15:35:19 -0400

    Allow width,height attributes in markdown

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -33,7 +33,7 @@ function sanitize(src) {
     return sanitizeHtml(src, {
         allowedTags: false, /* this means "all" */
         allowedAttributes: {
-            '*': ['href', 'align', 'style', 'bgcolor', 'alt', 'src', 'autoplay']
+            '*': ['href', 'align', 'style', 'bgcolor', 'alt', 'src', 'autoplay', 'width', 'height']
         },
         transformTags: {
             'script': 'error'
