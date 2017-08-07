online-go/online-go.com

commit 0a8cee3c1bdc75dc653f0896868b76e4694d0ea1

Begin adding the extra parameter to requests.get. This is so that URLs that contain an id number can work with the type-checking framework we have built for requests, and also so they can work with the seamless translation framework.

src/components/Chat/Chat.tsx
@@ -174,8 +174,8 @@ export class Chat extends React.Component<ChatProperties, any> {
     }
 
     resolve() {{{
-        if (!data.get("config.user").anonymous) {
-            get("me/groups", {page_size: 30})
+        if (data.get("user").anonymous) {
+            get("me/groups", 0, {page_size: 30})
             .then((groups) => {
                 this.setState({group_channels: groups.results.sort((a, b) => a.name.localeCompare(b.name))});
                 groups.results.map((g) => {
@@ -184,7 +184,7 @@ export class Chat extends React.Component<ChatProperties, any> {
             })
             .catch((err) => 0);
 
-            get("me/tournaments", {ended__isnull:true, page_size: 30})
+            get("me/tournaments", 0, {ended__isnull:true, page_size: 30})
             .then((tournaments) => {
                 this.setState({tournament_channels: tournaments.results.sort((a, b) => a.name.localeCompare(b.name))});
                 tournaments.results.map((t) => {

src/components/Announcements/Announcements.tsx
@@ -50,7 +50,7 @@ export class Announcements extends React.PureComponent<AnnouncementsProperties, 
     }
 
     componentWillMount() {{{
-        get("announcements")
+        get("announcements", 0)
         .then((announcements) => {
             for (let announcement of announcements) {
                 this.announce(announcement);

src/components/BlockPlayer/BlockPlayer.ts
@@ -72,7 +72,7 @@ function update_blocks() {
     let user = data.get("user");
 
     if (!user.anonymous) {
-        get("me/blocks")
+        get("me/blocks", 0)
         .then((entries) => {
             block_state = {};
             let new_ignores = {};

src/components/FriendList/FriendIndicator.tsx
@@ -70,7 +70,7 @@ export class FriendIndicator extends React.PureComponent<{}, any> {
     }
 
     refresh() {
-        get("ui/friends").then((res) => {
+        get("ui/friends", 0).then((res) => {
             data.set("friends", res.friends);
         }).catch((err) => {
             console.error("Error resolving friends list: ", err);

src/components/GroupAutocomplete/GroupAutocomplete.tsx
@@ -78,7 +78,7 @@ export class GroupAutocomplete extends React.PureComponent<GroupAutocompleteProp
         this.current_search = value;
 
         if (value.length > 1) {
-            get("groups/", {name__istartswith: value, page_size: 10})
+            get("groups/", 0, {name__istartswith: value, page_size: 10})
             .then((res) => {
                 for (let group of res.results) {
                     groups_by_name[group.name] = group;

src/components/LadderComponent/LadderComponent.tsx
@@ -76,7 +76,7 @@ export class LadderComponent extends React.PureComponent<LadderComponentProperti
 
 
     reload = () => {{{
-        get(`ladders/${this.props.ladderId}`)
+        get("ladders/%%", this.props.ladderId)
         .then((ladder) => this.setState({ladder: ladder}))
         .catch(errorAlerter);
 

src/components/ModerateUser/ModerateUser.tsx
@@ -48,7 +48,7 @@ export class ModerateUser extends Modal<ModerateUserProperties, any> {
 
     componentWillMount() {
         super.componentWillMount();
-        get(`players/${this.props.playerId}/full`)
+        get("players/%%/full", this.props.playerId)
         .then((dets) => {
             console.log(dets);
             this.setState(Object.assign({loading: false}, dets.user, {bot_owner: dets.user.bot_owner ? dets.user.bot_owner.id : null}));

src/components/NavBar/NavBar.tsx
@@ -65,7 +65,7 @@ function toggleTheme() {
 let setThemeLight = setTheme.bind(null, "light");
 let setThemeDark = setTheme.bind(null, "dark");
 function logout() {
-    get("/api/v0/logout").then((config) => {
+    get("/api/v0/logout", 0).then((config) => {
         data.set("config", config);
         window.location.reload();
     });
@@ -198,7 +198,7 @@ export class NavBar extends React.PureComponent<{}, any> {
                     omnisearch_groups: [],
                 });
 
-                get("ui/omniSearch", {q: q.trim()})
+                get("ui/omniSearch", 0, {q: q.trim()})
                 .then((res) => {
                     player_cache.update(res.players);
                     this.setState({

commit ab25e70c43614216e2bff3c21af5faf13c5070d9

Fixed missing group/tournament chat channel bug
Introduced in 0a8cee3c1bdc75dc653f0896868b76e4694d0ea1

src/components/Chat/Chat.tsx
@@ -174,7 +174,7 @@ export class Chat extends React.Component<ChatProperties, any> {
     }
 
     resolve() {{{
-        if (data.get("user").anonymous) {
+        if (!data.get("user").anonymous) {
             get("me/groups", {page_size: 30})
             .then((groups) => {
                 this.setState({group_channels: groups.results.sort((a, b) => a.name.localeCompare(b.name))});