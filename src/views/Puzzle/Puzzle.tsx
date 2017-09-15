online-go/online-go.com

commit 98e68fe451a862ecd94b4dfcbb36a5c8986de428

Puzzle refactoring

src/views/Puzzle/PuzzleEditing.ts
@@ -1,0 +1,337 @@
+/*
+ * Copyright (C) 2012-2017  Online-Go.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {Goban, GoMath} from "goban";
+import {errorAlerter, dup, ignore} from "misc";
+import {TransformSettings, PuzzleTransform} from './PuzzleTransform';
+import data from "data";
+import {abort_requests_in_flight, post, get, put, del} from "requests";
+import preferences from "preferences";
+
+
+export class PuzzleEditor {
+
+    // goban: Goban;
+    // goban_div: any;
+
+    orig_puzzle: any = null;
+    puzzle: any = null;
+
+    constructor(
+        private transform: PuzzleTransform
+    ) {}
+
+    clearPuzzles() {
+        this.orig_puzzle = null;
+        this.puzzle = null;
+    }
+
+    /**
+     * Edit puzzle and return new state
+     * @param new_puzzle True if it is a new puzzle
+     * @return State
+     */
+    private editPuzzle(new_puzzle: boolean): any {
+        this.transform.settings.reset();
+
+        let obj: any = {
+            editing: true,
+            edit_step: "setup",
+            setup_color: "black",
+            loaded: true,
+        };
+
+        if (new_puzzle) {
+            obj = Object.assign(obj, {
+                "id": 242,
+                "owner": data.get("user"),
+                "name": "",
+                "created": "",
+                "modified": "",
+                "puzzle": {
+                    "puzzle_player_move_mode": "free",
+                    "puzzle_rank": "18",
+                    "name": "",
+                    //"move_tree": { },
+                    "initial_player": "black",
+                    "puzzle_opponent_move_mode": "automatic",
+                    "height": 19,
+                    "width": 19,
+                    "mode": "puzzle",
+                    "puzzle_collection": 0,
+                    "puzzle_type": "life_and_death",
+                    "initial_state": {
+                        "white": "",
+                        "black": ""
+                    },
+                    "puzzle_description": ""
+                },
+                "private": false,
+                "width": 19,
+                "height": 19,
+                "type": "life_and_death",
+                "has_solution": false,
+                "rank": 18,
+                "collection": { },
+            });
+            this.orig_puzzle = obj.puzzle;
+            obj.puzzle_collection_summary = [];
+        }
+
+        return obj;
+    }
+
+    /**
+     * Create collection for puzzle
+     * NOTE: Does not catch error
+     * @param puzzle Puzzle
+     */
+    createPuzzleCollection(puzzle: any): Promise<any> {
+        let postResult;
+        return post("puzzles/collections/", {
+            "name": name,
+            "private": false,
+            "price": "0.00",
+        }).then((res) => {
+            postResult = res;
+            return get("puzzles/collections/", {page_size: 100, owner: data.get("user").id});
+        }).then((collections) => {
+            return {
+                puzzle: Object.assign({}, puzzle, {puzzle_collection: postResult.id}),
+                puzzle_collections: collections.results
+            };
+        });
+    }
+
+    /**
+     * Download a puzzle for given id
+     * @param puzzle_id puzzle id
+     * @param callback assignes new state and editing status
+     */
+    fetchPuzzle(puzzle_id: number, callback: (state: any, editing: boolean) => void) {{{
+        abort_requests_in_flight(`puzzles/`, "GET");
+        if (isNaN(puzzle_id)) {
+            get("puzzles/collections/", {page_size: 100, owner: data.get("user").id})
+            .then((collections) => {
+                callback(
+                    Object.assign(
+                        { puzzle_collections: collections.results },
+                        this.editPuzzle(true)
+                    ),
+                    true
+                );
+            })
+            .catch(errorAlerter);
+            return;
+        }
+
+        Promise.all([
+            get("puzzles/%%", puzzle_id),
+            get("puzzles/%%/collection_summary", puzzle_id),
+            get("puzzles/%%/rate", puzzle_id),
+        ])
+        .then((arr) => {
+            let rating = arr[2];
+            let puzzle = arr[0].puzzle;
+
+            let randomize_transform = preferences.get("puzzle.randomize.transform"); /* only randomize when we are getting a new puzzle */
+            let randomize_color = preferences.get("puzzle.randomize.color"); /* only randomize when we are getting a new puzzle */
+
+            this.transform.settings.zoom = preferences.get("puzzle.zoom");
+
+            this.transform.settings.transform_color = randomize_color && Math.random() > 0.5;
+            this.transform.settings.transform_h = randomize_transform && Math.random() > 0.5;
+            this.transform.settings.transform_v = randomize_transform && Math.random() > 0.5;
+            this.transform.settings.transform_x = randomize_transform && Math.random() > 0.5;
+
+            let new_state = Object.assign({
+                puzzle_collection_summary: arr[1],
+                loaded: true,
+                my_rating: rating.rating,
+                rated: !("error" in rating),
+                zoom: this.transform.settings.zoom,
+                transform_color: this.transform.settings.transform_color,
+                transform_h: this.transform.settings.transform_h,
+                transform_v: this.transform.settings.transform_v,
+                transform_x: this.transform.settings.transform_x,
+            }, arr[0]);
+
+            console.log("[PuzzleEditing].fetchPuzzle ==>", puzzle);
+
+            this.orig_puzzle = puzzle;
+
+            let bounds = this.getBounds(puzzle, puzzle.width, puzzle.height);
+            new_state.zoomable = bounds && (bounds.left > 0 || bounds.top > 0 || bounds.right < puzzle.width - 1 || bounds.bottom < puzzle.height - 1);
+
+            callback(new_state, false);
+        })
+        .catch(errorAlerter);
+    }}}
+
+    /**
+     * Reset board
+     * @param goban_div Goban Html Div component
+     * @param editing True if it is editing
+     * @return Goban options
+     */
+    reset(goban_div: any, editing: boolean, replacementSettingsFunction: () => object): any {{{
+        let puzzle = this.puzzle = dup(this.orig_puzzle);
+
+        if (!puzzle) {
+            throw new Error("No puzzle loaded");
+        }
+
+        if (!editing) {
+            this.transform.transformPuzzle(puzzle);
+        }
+        let bounds = this.transform.settings.zoom ? this.getBounds(puzzle, puzzle.width, puzzle.height) : null;
+        if (editing) {
+            bounds = null;
+        }
+
+        let label_position = preferences.get("label-positioning");
+
+        goban_div.empty();
+
+        let opts: any = Object.assign({
+            "board_div": goban_div,
+            "interactive": true,
+            //"onUpdate": sync,
+            "mode": "puzzle",
+            "draw_top_labels": (label_position === "all" || label_position.indexOf("top") >= 0),
+            "draw_left_labels": (label_position === "all" || label_position.indexOf("left") >= 0),
+            "draw_right_labels": (label_position === "all" || label_position.indexOf("right") >= 0),
+            "draw_bottom_labels": (label_position === "all" || label_position.indexOf("bottom") >= 0),
+            //"move_tree_div": "#game-move-tree-container",
+            //"move_tree_canvas": "#game-move-tree-canvas",
+            "getPuzzlePlacementSetting": () => {
+                return {"mode": "play"};
+            },
+            "bounds": bounds,
+            "player_id": 0,
+            "server_socket": null,
+
+            //"square_size": function(goban) { return getGobanSquareSize(goban); },
+            /*
+            "onCorrectAnswer": function() {
+                $scope.show_correct = true;
+                $scope.show_wrong = false;
+                if (!$scope.$$phase) $scope.$digest();
+                console.log("Correct");
+                setTimeout(function() {
+                    $("#next_link").focus();
+                }, 1);
+                logSuccess();
+            },
+            "onWrongAnswer": function() {
+                $scope.show_wrong = true;
+                $scope.show_correct = false;
+                if (!$scope.$$phase) $scope.$digest();
+                console.log("Wrong");
+                attempts++;
+            },
+            */
+
+           square_size: 4
+
+            //"display_width": Math.min(this.refs.goban_container.offsetWidth, this.refs.goban_container.offsetHeight),
+        }, puzzle);
+
+        let newState = null;
+
+        if (editing) {
+            opts.getPuzzlePlacementSetting = replacementSettingsFunction;
+            opts.puzzle_opponent_move_mode = "automatic";
+            opts.puzzle_player_move_mode = "free";
+            opts.puzzle_rank = puzzle && puzzle.puzzle_rank ? puzzle.puzzle_rank : 0;
+            opts.puzzle_collection = (puzzle && puzzle.collection ? puzzle.collection.id : 0);
+            opts.puzzle_type = (puzzle && puzzle.type ? puzzle.type : "");
+            opts.move_tree_div = "#move-tree-container";
+            opts.move_tree_canvas = "#move-tree-canvas";
+        }
+
+        return opts;
+    }}}
+
+
+    getBounds(puzzle, width, height) {{{
+        let ret = {
+            top: 9999,
+            bottom: 0,
+            left: 9999,
+            right: 0,
+        };
+
+        let process = (pos, width, height) => {
+            if (Array.isArray(pos)) {
+                for (let i = 0; i < pos.length; ++i) {
+                    process(pos[i], width, height);
+                }
+                return;
+            }
+
+            if (pos.x >= 0) {
+                ret.left   = Math.min(pos.x, ret.left);
+                ret.right  = Math.max(pos.x, ret.right);
+                ret.top    = Math.min(pos.y, ret.top);
+                ret.bottom = Math.max(pos.y, ret.bottom);
+            }
+
+            if (pos.marks && Array.isArray(pos.marks)) {
+                for (let i = 0; i < pos.marks.length; ++i) {
+                    process(pos.marks[i], width, height);
+                }
+            }
+
+            if (pos.branches) {
+                process(pos.branches, width, height);
+            }
+        };
+
+        process(GoMath.decodeMoves(puzzle.initial_state.black), width, height);
+        process(GoMath.decodeMoves(puzzle.initial_state.white), width, height);
+        process(puzzle.move_tree, width, height);
+
+        if (ret.top > ret.bottom) {
+            return null;
+        }
+
+        let padding = 1;
+        ret.top = Math.max(0, ret.top - padding);
+        ret.bottom = Math.min(height - 1, ret.bottom + padding);
+        ret.left = Math.max(0, ret.left - padding);
+        ret.right = Math.min(width - 1, ret.right + padding);
+
+        let snap_to_edge = 3;
+        if (ret.top <= snap_to_edge) {
+            ret.top = 0;
+        }
+        if (ret.bottom >= height - snap_to_edge) {
+            ret.bottom = height - 1;
+        }
+        if (ret.left <= snap_to_edge) {
+            ret.left = 0;
+        }
+        if (ret.right >= width - snap_to_edge) {
+            ret.right = width - 1;
+        }
+
+        return ret;
+    }}}
+
+
+}

Gulpfile.js
@@ -22,12 +22,12 @@ let ts_sources = ['src/**/*.ts', 'src/**/*.tsx'];
 gulp.task('default', ['dev-server', "livereload-server", "background_webpack", "build_styl", "watch_styl", "watch_dist_js", "watch_html", "watch_tslint"]);
 gulp.task('watch_dist_js', () => { gulp.watch(['dist/*.js'], livereload.reload); });
 gulp.task('watch_html', () => { gulp.watch(['src/*.html'], livereload.reload); });
-gulp.task('watch_styl', () => { gulp.watch(['src/**/*.styl', 'src/*.styl'], ['build_styl']); }); 
+gulp.task('watch_styl', () => { gulp.watch(['src/**/*.styl', 'src/*.styl'], ['build_styl']); });
 gulp.task('build_styl', build_styl);
 gulp.task('min_styl', min_styl);
 gulp.task('livereload-server', () => { livereload.listen(35701); });
 gulp.task('background_webpack', background_webpack);
-gulp.task('watch_tslint', () => { gulp.watch([ts_sources], ['tslint']); }); 
+gulp.task('watch_tslint', () => { gulp.watch([ts_sources], ['tslint']); });
 gulp.task('dev-server', dev_server);
 gulp.task('tslint', lint);
 
@@ -190,12 +190,12 @@ function dev_server(done) {
             res2.on('end', () => {
                 res.setHeader('content-type', 'application/javascript');
                 res.setHeader("Content-Length", data.length);
-                res.send(200, data);
+                res.status(200).send(data);
             });
         });
 
         req2.on('error', (e) => {
-            res.send(500, e.message);
+            res.status(500).send(e.message);
         });
 
         req2.end();
@@ -225,7 +225,7 @@ function dev_server(done) {
                 case 'VERSION_DOTJS': return 'js';
                 case 'VERSION_DOTCSS': return 'css';
                 case 'LANGUAGE_VERSION_DOTJS': return 'js';
-                case 'EXTRA_CONFIG': 
+                case 'EXTRA_CONFIG':
                     return `<script>window['websocket_host'] = "https://beta.online-go.com";</script>`
                 ;
             }

src/lib/ogs-goban/Goban.ts
@@ -3293,7 +3293,7 @@ export abstract class Goban extends EventEmitter {
             this.emit("update");
         }
     } /* }}} */
-    public showNext() { /* {{{ */
+    public showNext(dont_update_display?) { /* {{{ */
         if (this.mode === "conditional") {
             if (this.currently_my_cmove) {
                 if (this.current_cmove.move != null) {
@@ -3311,8 +3311,11 @@ export abstract class Goban extends EventEmitter {
             }
             this.engine.showNext();
         }
-        this.updateTitleAndStonePlacement();
-        this.emit("update");
+
+        if (!dont_update_display) {
+            this.updateTitleAndStonePlacement();
+            this.emit("update");
+        }
     } /* }}} */
     public prevSibling() { /* {{{ */
         let sibling = this.engine.cur_move.prevSibling();

src/views/Puzzle/PuzzleNavigation.ts
@@ -1,0 +1,86 @@
+/*
+ * Copyright (C) 2012-2017  Online-Go.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {Goban, GoMath} from "goban";
+
+export class PuzzleNavigation {
+
+    _goban: Goban;
+
+    constructor() {}
+
+    set goban(newValue: Goban) { this._goban = newValue; }
+
+    nav_up = () => {{{
+        this.checkAndEnterAnalysis();
+        this._goban.prevSibling();
+    }}}
+    nav_down = () => {{{
+        this.checkAndEnterAnalysis();
+        this._goban.nextSibling();
+    }}}
+    nav_first = () => {{{
+        this.checkAndEnterAnalysis();
+        this._goban.showFirst();
+    }}}
+    nav_prev_10 = () => {{{
+        this.checkAndEnterAnalysis();
+        for (let i = 0; i < 10; ++i) {
+            // update display only for final navigation result
+            this._goban.showPrevious( i < 9 );
+        }
+    }}}
+    nav_prev = () => {{{
+        this.checkAndEnterAnalysis();
+        this._goban.showPrevious();
+    }}}
+    nav_next = (event?: React.MouseEvent<any>) => {{{
+        this.checkAndEnterAnalysis();
+        this._goban.showNext();
+    }}}
+    nav_next_10 = () => {{{
+        this.checkAndEnterAnalysis();
+        for (let i = 0; i < 10; ++i) {
+            // update display only for final navigation result
+            this._goban.showNext( i < 9 );
+        }
+    }}}
+    nav_last = () => {{{
+        this.checkAndEnterAnalysis();
+        this._goban.jumpToLastOfficialMove();
+    }}}
+
+    checkAndEnterAnalysis() {{{
+        if (this._goban.mode === "puzzle") {
+            this._goban.setMode("analyze", true);
+            return true;
+        }
+        if (this._goban.mode === "analyze") {
+            return true;
+        }
+        return false;
+    }}}
+
+    checkAndEnterPuzzleMode() {{{
+        if (this._goban.mode !== "puzzle") {
+            this._goban.setAnalyzeTool("stone", "alternate");
+            this._goban.setMode("puzzle", true);
+        }
+        return true;
+    }}}
+
+}

src/views/Puzzle/PuzzleSettingsModal.tsx
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2012-2017  Online-Go.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as React from "react";
+import preferences from "preferences";
+import {_} from "translate";
+
+interface PuzzleSettingsModalProperties {
+}
+
+export class PuzzleSettingsModal extends React.PureComponent<PuzzleSettingsModalProperties, any> {
+    constructor(props) { /* {{{ */
+        super(props);
+        this.state = {
+            randomize_transform: preferences.get("puzzle.randomize.transform"),
+            randomize_color: preferences.get("puzzle.randomize.color"),
+        };
+    } /* }}} */
+
+    toggleTransform = () => {{{
+        preferences.set("puzzle.randomize.transform", !this.state.randomize_transform);
+        this.setState({randomize_transform: !this.state.randomize_transform});
+    }}}
+    toggleColor = () => {{{
+        preferences.set("puzzle.randomize.color", !this.state.randomize_color);
+        this.setState({randomize_color: !this.state.randomize_color});
+    }}}
+    render() {{{
+        return (
+            <div className="PuzzleSettingsModal">
+                <div className="details">
+                    <div className="option">
+                        <input id="transform" type="checkbox" checked={this.state.randomize_transform} onChange={this.toggleTransform} />
+                        <label htmlFor="transform">{_("Randomly transform puzzles")}</label>
+                    </div>
+                    <div className="option">
+                        <input id="color" type="checkbox" checked={this.state.randomize_color}  onChange={this.toggleColor} />
+                        <label htmlFor="color">{_("Randomize colors")}</label>
+                    </div>
+                </div>
+            </div>
+        );
+    }}}
+}

src/views/Puzzle/PuzzleTransform.ts
@@ -1,0 +1,214 @@
+/*
+ * Copyright (C) 2012-2017  Online-Go.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {GoMath} from "goban";
+
+export class TransformSettings {
+    constructor(
+        public transform_color: boolean = false,
+        public transform_h: boolean = false,
+        public transform_x: boolean = false,
+        public transform_v: boolean = false,
+        public zoom: boolean = false
+    ) {}
+
+    reset() {
+        this.transform_color = false;
+        this.transform_h = false;
+        this.transform_v = false;
+        this.transform_x = false;
+        this.zoom = false;
+    }
+
+    log() {
+        console.log(
+            this.transform_x
+           , this.transform_h
+           , this.transform_v
+           , this.transform_color,
+           this.zoom
+       );
+    }
+}
+
+export class PuzzleTransform {
+
+    constructor(
+        public settings: TransformSettings
+    ) {}
+
+    transformMoveText(puzzle, txt) {{{
+        if (this.settings.transform_color) {
+            let colors = {
+                "White" : "Black",
+                "Musta" : "Valkoinen",
+                "Negro" : "Blanco",
+                "Noir" : "Blanc",
+                "Czarny" : "Biały",
+                "Svart" : "Vit",
+            };
+
+            let utf8_colors = {
+                "Schwarz" : "Weiß",
+                "黑" : "白",
+                "Черные" : "Белые",
+            };
+
+
+            let t = "tttttttttttt";
+            let T = "TTTTTTTTTTTT";
+            let tr = /tttttttttttt/g;
+            let Tr = /TTTTTTTTTTTT/g;
+            for (let c1 in colors) {
+                let c2 = colors[c1];
+
+                let c1r = new RegExp("\\b" + c1 + "\\b", "gm");
+                let c2r = new RegExp("\\b" + c2 + "\\b", "gm");
+
+                let c1caser = new RegExp("\\b" + c1 + "\\b", "gmi");
+                let c2caser = new RegExp("\\b" + c2 + "\\b", "gmi");
+
+                let c1case = c1.toLowerCase();
+                let c2case = c2.toLowerCase();
+
+                txt = txt
+                        .replace(c1r, T)
+                        .replace(c1, T)
+                        .replace(c1caser, t)
+                        .replace(c2r, c1)
+                        .replace(c2, c1)
+                        .replace(c2caser, c1case)
+                        .replace(tr, c2case)
+                        .replace(Tr, c2);
+            }
+            for (let c1 in utf8_colors) {
+                let c2 = utf8_colors[c1];
+
+                txt = txt
+                        .replace(c1, T)
+                        .replace(c2, c1)
+                        .replace(Tr, c2);
+            }
+        }
+
+        txt = txt.replace(/\b([a-zA-Z][0-9]{1,2})\b/g, (match, contents, offset, s) => {
+            let dec = GoMath.decodeMoves(contents, puzzle.width, puzzle.height);
+            this.transformCoordinate(puzzle, dec[0], puzzle.width, puzzle.height);
+            let ret = GoMath.prettyCoords(dec[0].x, dec[0].y, puzzle.height);
+            if (/[a-z]/.test(contents)) {
+                return ret.toLowerCase();
+            } else {
+                return ret.toUpperCase();
+            }
+        });
+
+        return txt;
+    }}}
+
+    transformCoordinate(puzzle, coord, width, height) {{{
+        if (coord.marks && Array.isArray(coord.marks)) {
+            for (let i = 0; i < coord.marks.length; ++i) {
+                this.transformCoordinate(puzzle, coord.marks[i], width, height);
+            }
+        }
+        if (coord.text) {
+            coord.text = this.transformMoveText(puzzle, coord.text);
+        }
+
+        if (coord.x < 0) { return; }
+
+        if (this.settings.transform_x) {
+            let t = coord.y;
+            coord.y = coord.x;
+            coord.x = t;
+        }
+        if (this.settings.transform_h) { coord.x = (width - 1) - coord.x; }
+        if (this.settings.transform_v) { coord.y = (height - 1) - coord.y; }
+    }}}
+
+    transformCoordinates(puzzle, coords, width, height) {{{
+        if (Array.isArray(coords)) {
+            for (let i = 0; i < coords.length; ++i) {
+                this.transformCoordinate(puzzle, coords[i], width, height);
+                if (coords[i].branches) {
+                    this.transformCoordinates(puzzle, coords[i].branches, width, height);
+                }
+            }
+        } else {
+            this.transformCoordinate(puzzle, coords, width, height);
+            if (coords.branches) {
+                this.transformCoordinates(puzzle, coords.branches, width, height);
+            }
+        }
+        return coords;
+    }}}
+
+    transformPuzzle(puzzle) {{{
+        let width = puzzle.width;
+        let height = puzzle.height;
+
+        if (puzzle.initial_state && puzzle.initial_state.black && puzzle.initial_state.black.length) {
+            puzzle.initial_state.black = GoMath.encodeMoves(this.transformCoordinates(puzzle, GoMath.decodeMoves(puzzle.initial_state.black), width, height));
+        }
+        if (puzzle.initial_state && puzzle.initial_state.white && puzzle.initial_state.white.length) {
+            puzzle.initial_state.white = GoMath.encodeMoves(this.transformCoordinates(puzzle, GoMath.decodeMoves(puzzle.initial_state.white), width, height));
+        }
+        if (puzzle.move_tree) {
+            this.transformCoordinates(puzzle, puzzle.move_tree, width, height);
+        }
+
+        if (this.settings.transform_color) {
+            let t = puzzle.initial_state.black;
+            puzzle.initial_state.black = puzzle.initial_state.white;
+            puzzle.initial_state.white = t;
+
+            if (puzzle.initial_player === "black") {
+                puzzle.initial_player = "white";
+            } else {
+                puzzle.initial_player = "black";
+            }
+        }
+
+        if (puzzle.puzzle_description) {
+            puzzle.puzzle_description = this.transformMoveText(puzzle, puzzle.puzzle_description);
+        }
+    }}}
+
+
+    /**
+     * Return state for Puzzle class, based on given transformation
+     * @param what transformation key
+     * @return Puzzle State
+     */
+    stateForTransformation(what): any {{{
+        let state: any = null;
+
+        switch (what) {
+            case "h"     : state = {transform_h     : this.settings.transform_h     = !this.settings.transform_h};     break;
+            case "v"     : state = {transform_v     : this.settings.transform_v     = !this.settings.transform_v};     break;
+            case "x"     : state = {transform_x     : this.settings.transform_x     = !this.settings.transform_x};     break;
+            case "color" : state = {transform_color : this.settings.transform_color = !this.settings.transform_color}; break;
+            case "zoom"  :
+                state = {zoom: this.settings.zoom = !this.settings.zoom};
+            break;
+        }
+
+       return state;
+    }}}
+
+
+}

webpack.config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log(process.env.NODE_PATH)
+console.log('node path :', process.env.NODE_PATH)
 
 var path = require('path');
 let fs = require('fs');
@@ -41,8 +41,8 @@ module.exports = {
     },
 
     performance: {
-        maxAssetSize: 1024*1024*2.5,
-        maxEntrypointSize: 1024*1024*2.5,
+        maxAssetSize: 1024 * 1024 * 2.5,
+        maxEntrypointSize: 1024 * 1024 * 2.5,
     },
 
     plugins: [
@@ -59,8 +59,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.`
-    )
+along with this program.  If not, see <http://www.gnu.org/licenses/>.`)
     ],
 
     //devtool: 'eval',

commit ef70015c4dfb5baeffbd85a57de729f02a665c42

Fixed puzzle collection creation
Fixes #439

Broken in 98e68fe451a862ecd94b4dfcbb36a5c8986de428 (PR #393)

Still not sure why the compiler didn't catch this.

src/views/Puzzle/Puzzle.tsx
@@ -402,7 +402,7 @@ export class Puzzle extends React.Component<PuzzleProperties, any> {
                     return;
                 }
 
-                this.editor.createPuzzleCollection(this.state.puzzle)
+                this.editor.createPuzzleCollection(this.state.puzzle, name)
                     .then((state) => this.setState(state))
                     .catch(errorAlerter);
             })

src/views/Puzzle/PuzzleEditing.ts
@@ -100,7 +100,7 @@ export class PuzzleEditor {
      * NOTE: Does not catch error
      * @param puzzle Puzzle
      */
-    createPuzzleCollection(puzzle: any): Promise<any> {
+    createPuzzleCollection(puzzle: any, name:string): Promise<any> {
         let postResult;
         return post("puzzles/collections/", {
             "name": name,