8f7d4ac5739b41a3a9cd6da584b4e3a2c61c23ad
Martin Gregory <mgregory@frabjous.fritz.box>
2017-10-02 13:35:06 +1030

    Fix exception on game creation - initialise state.score so the initial score can be displayed.  (#454)

--- src/views/Game/Game.tsx
+++ src/views/Game/Game.tsx
@@ -449,6 +449,11 @@ export class Game extends React.PureComponent<GameProperties, any> {
             this.goban.setMode("analyze");
         }
 
+        // We need an initial score for the first display rendering (which is not set in the constructor).
+        // Best to get this from the engine, so we know we have the right structure...
+        this.state.score = this.goban.engine.computeScore(true);
+
+
         /* Title Updates {{{ */
         let last_title = window.document.title;
         this.last_move_viewed = 0;
