7981261a17f0482e4166d045dcad1ccebcfdcd25
Akita Noek <anoek@online-go.com>
2017-11-22 15:41:27 -0700

    Updated supporter goal text for Asia goal

--- src/components/SupporterGoals/SupporterGoals.tsx
+++ src/components/SupporterGoals/SupporterGoals.tsx
@@ -46,22 +46,22 @@ export class SupporterGoals extends React.Component<SupporterGoalsProperties, an
         }
 
 
-        let adfree_done = goals.adfree * 80;
-        let adfree_left = (1.0 - goals.adfree) * 80;
-        let loc1_done = goals.loc1 * 10;
-        let loc1_left = (1.0 - goals.loc1) * 10;
-        let loc2_done = goals.loc2 * 10;
-        let loc2_left = (1.0 - goals.loc2) * 10;
+        let adfree_done = goals.adfree * 50;
+        let adfree_left = (1.0 - goals.adfree) * 50;
+        let loc1_done = goals.loc1 * 35;
+        let loc1_left = (1.0 - goals.loc1) * 35;
+        let loc2_done = goals.loc2 * 15;
+        let loc2_left = (1.0 - goals.loc2) * 15;
 
-        //let loc1_text = _("Asia") + ` (${(goals.loc1 * 100.0).toFixed(1)}%)`;
+        let loc1_text = _("Asia") + ` (${(goals.loc1 * 100.0).toFixed(1)}%)`;
         //let loc2_text = _("Europe") + ` (${(goals.loc2 * 100.0).toFixed(1)}%)`;
-        let loc1_text = _("Asia");
+        //let loc1_text = _("Asia") + ;
         let loc2_text = _("Europe");
 
         return (
             <div id='SupporterGoalsContainer'>
                 <div id='SupporterGoalText'>
-                    <Link to='/user/supporter'>{_("We would like to get rid of all ads on the site, as well as add some additional servers in different parts of the world for better service. We need more supporters to do this, so chip in today for a better tomorrow!")} :)</Link>
+                    <Link to='/user/supporter'>{_("Thanks to our supporters, we have met our first goal of being able to be completely ad free! Our two remaining financial goals are to be able to afford servers in Asia and Europe, which will greatly improve service around the world. Please consider chipping in!")} :)</Link>
                 </div>
 
                 <div id='SupporterGoals'>
@@ -69,7 +69,7 @@ export class SupporterGoals extends React.Component<SupporterGoalsProperties, an
                         <div className={`progress-bar ${goals.adfree >= 1 ? 'success' : 'primary'}`} style={{width: `${adfree_done}%`}}>Ad Free OGS! ({(goals.adfree * 100.0).toFixed(1)}%)</div>
                         {goals.adfree < 1 && <div className={`progress-bar default`} style={{width: `${adfree_left}%`}}>&nbsp;</div>}
                         <div className={`progress-bar ${goals.loc1 >= 1 ? 'success' : 'primary'}`} style={{width: `${loc1_done}%`}}>{goals.loc1 > 0.5 ? loc1_text : <span>&nbsp;</span>}</div>
-                        {goals.loc1 < 1 && <div className={`progress-bar info`} style={{width: `${loc1_left}%`}}>{goals.loc1 <= 0.5 ? loc1_text : <span>&nbsp;</span>}</div> }
+                        {goals.loc1 < 1 && <div className={`progress-bar default`} style={{width: `${loc1_left}%`}}>{goals.loc1 <= 0.5 ? loc1_text : <span>&nbsp;</span>}</div> }
                         <div className={`progress-bar ${goals.loc2 >= 1 ? 'success' : 'primary'}`} style={{width: `${loc2_done}%`}}>{goals.loc2 > 0.5 ? loc2_text : <span>&nbsp;</span>}</div>
                         {goals.loc2 < 1 && <div className={`progress-bar info`} style={{width: `${loc2_left}%`}}>{goals.loc2 <= 0.5 ? loc2_text :  <span>&nbsp;</span>}</div> }
                     </div>
