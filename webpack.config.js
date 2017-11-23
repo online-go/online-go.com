a05a9a05dbd3ff130446ad18ddba802d66bd5a0e
Akita Noek <anoek@online-go.com>
2017-11-22 23:10:55 -0700

    Revert "Attempt at fixing invariant error"
    This reverts commit a7fbbfa4371fded4a58b26dbad61145ae88ce92c.
    
    this had nothing to do with the issue

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,6 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 `));
 
-    /*
 plugins.push(
     new webpack.optimize.CommonsChunkPlugin({
         name: 'vendor',
@@ -38,7 +37,6 @@ plugins.push(
         }
     })
 );
-*/
 
 if (production) {
     plugins.push(
