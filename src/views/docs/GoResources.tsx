aa36632db03678c2e5a080127fa34bdbec525263
Mariano López Minnucci <mlopezviedma@gmail.com>
2018-03-13 14:58:43 -0300

    Add Sabaki to resources page

--- src/views/docs/GoResources.tsx
+++ src/views/docs/GoResources.tsx
@@ -318,6 +318,7 @@ export let GoResources = (props) => {
                 <span><i className="fa fa-linux"/> <i className="fa fa-apple"/> <i className="fa fa-windows"></i> <a href="https://www.gnu.org/software/gnugo/gnugo.html">GNU Go</a></span>,
                 <span><i className="fa fa-linux"/> <i className="fa fa-apple"/> <i className="fa fa-windows"></i> <a href="http://pachi.or.cz/">Pachi</a></span>,
                 <span><i className="fa fa-linux"/> <i className="fa fa-apple"/> <i className="fa fa-windows"></i> <a href="http://gogui.sourceforge.net/">GoGui</a></span>,
+                <span><i className="fa fa-linux"></i> <i className="fa fa-apple"></i> <i className="fa fa-windows"></i> <a href="https://github.com/SabakiHQ/Sabaki">Sabaki</a></span>,
                 <span><i className="fa fa-android"/> <a href="https://play.google.com/store/apps/details?id=net.lrstudios.android.pachi">Pachi for Android</a></span>,
                 <span><i className="fa fa-android"/> <a href="https://play.google.com/store/apps/details?id=nl.tengen.gridmaster">Go GridMaster</a></span>,
                 <span><i className="fa fa-android"/> <a href="https://play.google.com/store/apps/details?id=org.ligi.gobandroid_hd">Gobandroid HD</a></span>,
