242498755c38c40ad7005851cddbaf6b4093518f
GreenAsJade <martin@grgories.net>
2018-12-01 13:36:24 +1030

    Oh 2nd time in a row!  I have to get my editor to call these out!

--- src/components/IncidentReportTracker/IncidentReportTracker.tsx
+++ src/components/IncidentReportTracker/IncidentReportTracker.tsx
@@ -205,7 +205,7 @@ export class IncidentReportTracker extends React.PureComponent<IncidentReportTra
                             {this.state.reports.map((report, idx) => (
                                 <div className="incident" key={report.id}>
                                     <div className="report-header">
-                                        <div className="report-id">{"#"+report.id+":"}</div>
+                                        <div className="report-id">{"#" + report.id + ":"}</div>
                                         {report.moderator ?
                                             <Player user={report.moderator} icon />
                                             :
