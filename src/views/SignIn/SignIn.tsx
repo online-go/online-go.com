5b2634af3e6242fa90e01a83bb3c57587045cade
Akita Noek <anoek@online-go.com>
2019-04-02 09:36:46 -0600

    Inlined icons; Add i18n
    This patch inlines the google and facebook icons into our stylesheet,
    wraps new login text with our translation functions, replaces the
    twitter svg and email icon svg with the seemingly identical font awesome
    font icons we already have, and switches the email sign from <a> to
    <Link> since that's a UI side navigation.

--- src/views/SignIn/SignIn.tsx
+++ src/views/SignIn/SignIn.tsx
@@ -184,9 +184,9 @@ export class SignIn extends React.PureComponent<{}, any> {
                             <LineText>{
                                 _("or log in using another account:") /* translators: username or password, or sign in with social authentication */
                             }</LineText>
-                            <a href="/login/google/" className="s btn md-icon"><img src="/google-icon.svg" className="google" alt="Google" /> Login with Google</a>
-                            <a href="/login/facebook/" className="s btn md-icon"><img src="/facebook-icon.svg" className="facebook" alt="Facebook" /> Login with Facebook</a>
-                            <a href="/login/twitter/" className="s btn md-icon"><img src="/md-twitter.svg" className="twitter" alt="Twitter" />Login with Twitter</a>
+                            <a href="/login/google/" className="s btn md-icon"><span className="google google-icon" /> {_("Login with Google")}</a>
+                            <a href="/login/facebook/" className="s btn md-icon"><span className="facebook facebook-icon" /> {_("Login with Facebook")}</a>
+                            <a href="/login/twitter/" className="s btn md-icon"><i className="twitter twitter-icon fa fa-twitter" />{_("Login with Twitter")}</a>
                         </div>
                     </Card>
 
