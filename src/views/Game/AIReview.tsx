2ab4a2e291dd183cfc1d40d7d30d99beb31ca72a
Akita Noek <anoek@online-go.com>
2019-05-14 07:10:51 -0600

    Updated copyright year

--- src/views/Game/AIReview.tsx
+++ src/views/Game/AIReview.tsx
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2012-2017  Online-Go.com
+ * Copyright (C) 2012-2019  Online-Go.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const production = process.env.PRODUCTION ? true : false;
 let plugins = [];
 
 plugins.push(new webpack.BannerPlugin(
-`Copyright (C) 2012-2017  Online-Go.com
+`Copyright (C) 2012-2019  Online-Go.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
