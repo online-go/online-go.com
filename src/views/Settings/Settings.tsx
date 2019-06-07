3a104a8ab1afef8ade92d19dfd1680736f1ae740
Akita Noek <anoek@online-go.com>
2019-06-07 09:17:55 -0600

    Allow upper case letters in email addresses

--- src/views/Settings/Settings.tsx
+++ src/views/Settings/Settings.tsx
@@ -380,7 +380,7 @@ export class Settings extends React.PureComponent<{}, any> {
         if (this.state.profile.email.trim() === "") {
             return true;
         }
-        let re = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+        let re = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
         return re.test(this.state.profile.email.trim());
     }}}
 
