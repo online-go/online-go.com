d7a6a9e957d48cfc178c17f9561a170af08019dd
BHydden <runifyouseeme@gmail.com>
2019-09-03 22:42:42 +1000

    resolve conflicts attempt 2

--- src/views/Overview/Overview.tsx
+++ src/views/Overview/Overview.tsx
@@ -39,7 +39,7 @@ import {EmailBanner} from "EmailBanner";
 import {SupporterGoals} from "SupporterGoals";
 import {ProfileCard} from "ProfileCard";
 import {notification_manager} from "Notifications";
-import {ActiveAnnouncements} from "Announcements:;
+import {ActiveAnnouncements} from "Announcements";
 import {FabX, FabCheck} from "material";
 import {ServerTimeDisplay} from "ServerTimeDisplay";
 
@@ -124,7 +124,7 @@ export class Overview extends React.Component<{}, any> {
             <div id="Overview">
                 <div className="left">
                     <EmailBanner />
-                    <ActiveAnnouncements />
+                    <ActiveAnnouncements  />
                     <ServerTimeDisplay />
                     <ChallengesList onAccept={() => this.refresh()} />
 
