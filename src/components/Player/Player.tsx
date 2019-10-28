f6fc5bc7055fbeb257ba68b1365265d77926100a
flovo <4864182+flovo@users.noreply.github.com>
2019-10-28 11:48:17 +0100

    require explicit declaring of username to fetch player by username

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -72,7 +72,7 @@ export class Player extends React.PureComponent<PlayerProperties, any> {
             }
 
             let player_id = typeof(this.props.user) !== "object" ? this.props.user : (this.props.user.id || this.props.user.player_id) ;
-            let username = typeof(this.props.user) !== "object" ? this.props.user : this.props.user.username ;
+            let username = typeof(this.props.user) !== "object" ? null : this.props.user.username ;
             if (player_id && player_id > 0) {
                 player_cache.fetch(player_id, ["username", "ui_class", "ranking", "pro"]).then((user) => {
                     let player_id = typeof(this.props.user) !== "object" ? this.props.user : (this.props.user.id || this.props.user.player_id) ;
@@ -128,7 +128,7 @@ export class Player extends React.PureComponent<PlayerProperties, any> {
 
         if (!new_props.disableCacheUpdate) {
             let player_id = typeof(new_props.user) !== "object" ? new_props.user : (new_props.user.id || new_props.user.player_id) ;
-            let username = typeof(this.props.user) !== "object" ? this.props.user : this.props.user.username ;
+            let username = typeof(this.props.user) !== "object" ? null : this.props.user.username ;
 
             if (typeof(new_props.user) === "object" && new_props.user.id > 0) {
                 player_cache.update(new_props.user);
