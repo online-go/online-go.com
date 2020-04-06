6276a4028b393eedd7974c436affefcab38accab
Akita Noek <anoek@online-go.com>
2020-04-06 06:49:57 -0600

    Fixed headers not working in markdown

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -40,6 +40,12 @@ function sanitize(src) {
             'br',
             'details',
             'div',
+            'h1',
+            'h2',
+            'h3',
+            'h4',
+            'h5',
+            'h6',
 
             'header',
             'hgroup',
