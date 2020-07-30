9c566d689b4715550035cdecb401323b88e54e7e
Andrew Simons <andrewsimons@bubblegroup.com>
2020-07-30 09:31:03 +0100

    Add ability to add move number on end of game url to automatically skip to that move upon loading game, e.g. online-go.com/game/24010741/123 will skip to move 123. setTimeout was initially me being lazy to find where to put code to run after the normal logic to skip to last move of game, but actually gives nice visual clue that you are not viewing last move but skipped earlier IMO.

--- src/views/Game/Game.tsx
+++ src/views/Game/Game.tsx
@@ -712,6 +712,13 @@ export class Game extends React.PureComponent<GameProperties, any> {
             })
             .catch(ignore);
         }
+
+        const move_number_match = document.URL.match(/game\/\d+\/(\d+)$/);
+        if (move_number_match) {
+            setTimeout(() => {
+                this.nav_goto_move(Number(move_number_match[1]));
+            }, 1000);
+        }
     }
     private bindAudioEvents():void { // called by init
         let user = data.get('user');
