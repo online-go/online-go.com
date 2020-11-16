bf391c199e6cbb88515cfbf823fbc2aa9cfb36af
GreenAsJade <martin@gregories.net>
2020-11-17 08:52:30 +1030

    Prevent the user's rank from wrapping in game history table.

--- src/views/User/User.tsx
+++ src/views/User/User.tsx
@@ -903,7 +903,7 @@ export class User extends React.PureComponent<UserProperties, any> {
                                     orderBy={["-ended"]}
                                     groom={game_history_groomer}
                                     columns={[                                /* I wish we could set properties at the row level! */
-                                        {header: _("User"), className: (X) => ("color" +         ((X && X.annulled) ? " annulled" : "")),     render: (X) => ((X.played_black ? "⚫ " : "⚪ ")) + "[" + rankString(X.player) + "]"},
+                                        {header: _("User"), className: (X) => ("user_info" +         ((X && X.annulled) ? " annulled" : "")),     render: (X) => ((X.played_black ? "⚫ " : "⚪ ")) + "[" + rankString(X.player) + "]"},
                                         {header: _(""),       className: (X) => ("winner_marker" + ((X && X.annulled) ? " annulled" : "")),     render: (X) => (X.player_won ?  <i className="fa fa-trophy game-history-winner"/> : "")},
                                         {header: _("Date"),   className: (X) => ("date" +          ((X && X.annulled) ? " annulled" : "")),     render: (X) => moment(X.date).format("YYYY-MM-DD")},
                                         {header: _("Opponent"),  className: (X) => ("player" +     ((X && X.annulled) ? " annulled" : "")),     render: (X) => <Player user={X.opponent} disableCacheUpdate />} ,
