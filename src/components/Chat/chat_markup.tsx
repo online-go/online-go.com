031bdec1ff6cde64c23c1c9b1d123c53aa7a96c1
mattetress <mattetress@me.com>
2021-01-29 13:36:03 -0600

    add length limit arguement and apply to game chat

--- src/components/Chat/chat_markup.tsx
+++ src/components/Chat/chat_markup.tsx
@@ -184,9 +184,13 @@ const global_replacements: TextReplacement[] = [
     { split: /(https?:\/\/[^<> ]+)/i, pattern: /(https?:\/\/[^<> ]+)/i, replacement: (m, idx) => (<a key={idx} target="_blank" href={m[1]}>{m[1]}</a>) },
 ];
 
-export function chat_markup(body: string, extra_pattern_replacements?: Array<TextReplacement>): Array<JSX.Element> {
+export function chat_markup(body: string, extra_pattern_replacements?: Array<TextReplacement>, lengthLimit?: integer): Array<JSX.Element> {
     let replacements = global_replacements;
 
+    if (lengthLimit && body.length > lengthLimit) {
+        return [<span key="message-too-long">&lt;{_("Message too long")}&gt;</span>];	
+    }	
+        
     if (extra_pattern_replacements) {
         replacements = replacements.concat(extra_pattern_replacements);
     }
