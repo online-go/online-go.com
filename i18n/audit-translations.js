07251a7f9e8ec9834ab0b8c9a06f8ac065121fea
Akita Noek <anoek@online-go.com>
2021-09-07 07:49:29 -0600

    Look for obvious vandalizations in translations

--- i18n/audit-translations.js
+++ i18n/audit-translations.js
@@ -18,6 +18,7 @@ function main() {
     let audit_promises = [];
     let write_promises = [];
     let progress = {};
+    let vandalized_languages = {};
 
     for (let lang in languages) {
         let conv = lang.replace(/([a-z]+)-([a-zA-Z]+)/, (_, a, b)=>`${a}_${b.toUpperCase()}`);
@@ -32,6 +33,7 @@ function main() {
                 let missing = 0;
                 let missing_strings = [];
                 let errors = 0;
+                let vandalizations = 0;
                 for (let item of po.items) {
                     if (item.obsolete) {
                         continue;
@@ -50,6 +52,11 @@ function main() {
                                 missing++;
                                 missing_strings.push(str);
                                 break;
+                            }
+
+                            if (looks_vandalized(lang, msg)) {
+                                console.log(`${conv}.po: vandalization: ${str} -> ${msg}`);
+                                vandalized_languages[lang] = ++vandalizations;
                             }
 
                             let m = msg.match(/%[sd]/g);
@@ -78,7 +85,9 @@ function main() {
                     }
                 }
 
-                if (errors > 0) {
+                if (vandalizations > 0) {
+                    console.log(`*** VANDALIZED *** ${vandalizations} vandalizations`);
+                } else if (errors > 0) {
                     console.log(`** BAD ** ${lang}: ${missing} missing ${errors} errors`);
                     //exit_code = 1;
                 } else if (missing > 0) {
@@ -92,27 +101,42 @@ function main() {
                     console.log(`GOOD ${lang}`);
                 }
 
+
                 progress[lang] = missing; 
 
-                write_promises.push(new Promise((resolve, reject) => {
-                    fs.readFile(`./locale/${lang}.js`, 'utf8', (err, data) => {
-                        if (/ogs_missing_translation_count/.test(data)) {
-                            data.replace(/ogs_missing_translation_count = [0-9]+/, `ogs_missing_translation_count = ${missing}`);
-                        } else {
-                            data += `window.ogs_missing_translation_count = ${missing};`;
-                        }
-                        fs.writeFile(`./locale/${lang}.js`, data, resolve);
-                    })
-                }));
+                if (vandalizations === 0) {
+
+                    write_promises.push(new Promise((resolve, reject) => {
+                        fs.readFile(`./locale/${lang}.js`, 'utf8', (err, data) => {
+                            if (/ogs_missing_translation_count/.test(data)) {
+                                data.replace(/ogs_missing_translation_count = [0-9]+/, `ogs_missing_translation_count = ${missing}`);
+                            } else {
+                                data += `window.ogs_missing_translation_count = ${missing};`;
+                            }
+                            fs.writeFile(`./locale/${lang}.js`, data, resolve);
+                        })
+                    }));
+                }
 
                 resolve();
             });
         }));
     }
 
+
+
+
+
     Promise.all(audit_promises).then(() => {
         Promise.all(write_promises).then(() => {
             fs.writeFile('./locale/translation_progress.json', JSON.stringify(progress), () => {
+
+                if (Object.keys(vandalized_languages).length > 0) {
+                    console.error(`Critical error: ${Object.keys(vandalized_languages).length} languages have been vandalized`);
+                    console.error(JSON.stringify(vandalized_languages, undefined, 4));
+                    process.exit(1);
+                }
+
                 process.exit(exit_code);
             });
         }).catch((err) => console.error(err));
@@ -121,6 +145,62 @@ function main() {
     });
     });
     });
+}
+
+function encode(obj) {
+    return btoa(unescape(encodeURIComponent(JSON.stringify(obj)))).replace(/.{76}(?=.)/g,'$&');
+}
+
+function decode(str) {
+    return JSON.parse(decodeURIComponent(escape(atob(str))));
+}
+
+let bad_symbols = decode(
+    'WyLljZAiLCLikrYiLCLimK0iLCLinK8iLCLimK4iLCLinKEiLCLljY0iLCLgv5UiLCLgv5YiLCLi' +
+    'nJkiLCLgv5ciLCLgv5giLCLqlqYiLCLwn4+0Iiwi4ZuL4ZuLIl0='
+);
+
+let profanity_regex = {};
+
+let profanity_dictionary = decode(
+    'eyJlbiI6WyJuaWdnZXIiLCJjdWNrIiwiZnVjayIsInNoaXQiLCJuYXppIiwiMTQ4OCIsImhpdGxl' +
+    'ciIsInN0cmFpZ2h0IHByaWRlIiwiZ2F5IHByaWRlIiwiZ2F5IiwiZ2F5cyJdfQ=='
+);
 
 
+for (let lang in profanity_dictionary) {
+console.log("(" + profanity_dictionary[lang].map(s => "\\b" + s + "\\b").join("|") + ")");
+    profanity_regex[lang] = new RegExp(
+        "(" + profanity_dictionary[lang].map(s => "\\b" + s + "\\b").join("|") + ")"
+        , "gui"
+    );
+}
+
+function looks_vandalized(lang, msg) {
+    if (detect_profanity("en", msg)) {
+        console.error("en Profanity detected");
+        return true;
+    }
+    if (detect_profanity(lang, msg)) {
+        console.error(lang, " Profanity detected");
+        return true;
+    }
+
+    let lower = msg.toLowerCase();
+
+    for (let entry of bad_symbols) {
+        if (lower.indexOf(entry) >= 0) {
+            return true;
+        }
+    }
+
+    return false
+}
+
+function detect_profanity(lang, msg) {
+    if (lang in profanity_regex) {
+        return profanity_regex[lang].test(msg);
+    }
+
+    return false;
 }
