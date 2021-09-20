0074af957925722cbe78c34b33f2d0d0789bd8a6
Benjamin Jones <benjaminpaikjones@gmail.com>
2021-09-20 23:45:03 +0900

    Change deprecated id-blacklist to id-denylist.
    See https://github.com/typescript-eslint/tslint-to-eslint-config/pull/1221 for details.

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,7 +68,7 @@ module.exports = {
             "error",
             "smart"
         ],
-        "id-blacklist": [
+        "id-denylist": [
             "error",
             "any",
             "Number",
