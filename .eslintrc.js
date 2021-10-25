d7d1c23c4b1dc934c7f21f986a377c05473bcce7
Benjamin Jones <benjaminpaikjones@gmail.com>
2021-10-25 09:28:41 -0400

    Suppress recommended rules that still have errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,34 @@ module.exports = {
         "plugin:@typescript-eslint/recommended"
     ],
     "rules": {
+        // Recommended rules with errors
+        // TODO: Fix these and re-enable them
+
+        "@typescript-eslint/ban-types": "off", // 35 errors
+        "@typescript-eslint/explicit-module-boundary-types": "off", // 2074 warnings
+        "@typescript-eslint/no-empty-function": "off", // 14 errors
+        "@typescript-eslint/no-empty-interface": "off", // 45 errors
+        "@typescript-eslint/no-explicit-any": "off", // 535 warnings
+        "@typescript-eslint/no-namespace": "off", // 1 error
+        "@typescript-eslint/no-this-alias": "off", // 4 errors
+        "@typescript-eslint/no-unused-vars": "off", // 675 warnings
+        "@typescript-eslint/triple-slash-reference": "off", // 1 error
+
+        "no-case-declarations": "off", // 3 errors
+        "no-constant-condition": "off", // 1 error
+        "no-empty": "off", // 30 errors
+        "no-empty-pattern": "off", // 1 error
+        "no-fallthrough": "off", // 3 errors
+        "no-misleading-character-class": "off", // 2 errors
+        "no-prototype-builtins": "off", // 8 errors
+        "no-self-assign": "off", // 2 errors
+        "no-useless-escape": "off", // 24 errors
+        "prefer-const": "off", // 6 errors
+        "prefer-spread": "off", // 7 errors
+
+        //==============================================================
+
+
         "@typescript-eslint/adjacent-overload-signatures": "error",
         "@typescript-eslint/consistent-type-assertions": "error",
         "@typescript-eslint/member-delimiter-style": "error",
