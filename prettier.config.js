b9402a203563544786a3afb6ea3a6389dcf440f3
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-01-01 21:36:39 -0800

    Remove most options from prettier config.

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,4 @@
-// prettier.config.js or .prettierrc.js
 module.exports = {
     trailingComma: "all",
     tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-    printWidth: 140,
-    quoteProps: "consistent",
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    arrowParens: "avoid",
 };
