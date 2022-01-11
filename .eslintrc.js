62187c3c7ce9d927cfcf010d1970f7cc8d908e83
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-01-11 12:50:46 -0800

    Remove formatting rules from eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,6 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": [
-        "prettier",
-    ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "project": "tsconfig.json",
@@ -18,7 +15,8 @@ module.exports = {
     ],
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
     ],
     "rules": {
         // Recommended rules with errors
@@ -63,11 +61,6 @@ module.exports = {
         "@typescript-eslint/prefer-namespace-keyword": "error",
         "@typescript-eslint/semi": "error",
         "@typescript-eslint/type-annotation-spacing": "error",
-        "brace-style": [
-            "error",
-            "1tbs",
-            { "allowSingleLine": true }
-        ],
         "computed-property-spacing": ["error", "never"],
         "curly": "error",
         "eol-last": "error",
@@ -88,15 +81,6 @@ module.exports = {
             "undefined"
         ],
         "id-match": "error",
-        "indent": [
-            "error",
-            4,
-            {
-                "SwitchCase": 1,
-                "MemberExpression": "off",
-                "ignoredNodes": ["ConditionalExpression"]
-            }
-        ],
         "jsdoc/check-alignment": "error",
         "jsdoc/require-asterisk-prefix": "error",
         "linebreak-style": [
@@ -131,10 +115,6 @@ module.exports = {
         "prefer-arrow-callback": [
             "error",
             { "allowNamedFunctions": true }
-        ],
-        "space-before-function-paren": [
-            "error",
-            "never"
         ],
         "use-isnan": "error",
         "@typescript-eslint/tslint/config": [
