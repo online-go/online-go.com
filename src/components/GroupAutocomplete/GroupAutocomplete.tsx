0c81eefe987023204951f9f19f5e3ba635e8a302
Daniel Saxton <dsaxton@pm.me>
2022-01-19 08:31:03 -0600

    Fix

--- src/components/GroupAutocomplete/GroupAutocomplete.tsx
+++ src/components/GroupAutocomplete/GroupAutocomplete.tsx
@@ -133,8 +133,7 @@ export class GroupAutocomplete extends React.PureComponent<
     };
 
     render() {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { suggestions, value } = this.state;
+        const { value } = this.state;
 
         const inputProps = {
             placeholder: this.props.placeholder || _("Group name"),
