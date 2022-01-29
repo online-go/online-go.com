a259b973949978b2112e6d3f1c2bbd0c9cb21a97
GreenAsJade <greenasjade@gregories.net>
2022-01-29 17:46:26 +1030

    Fix up componentWillUpdate logic.

--- src/components/TimeControl/TimeControlPicker.tsx
+++ src/components/TimeControl/TimeControlPicker.tsx
@@ -58,13 +58,20 @@ export class TimeControlPicker extends React.PureComponent<
         let update: TimeControl;
         // console.log("Timepicker props", next_props);
         if (
-            this.props.value !== next_props.value ||
-            this.props?.force_system !== this.state.system
+            next_props?.value !== this.props.value ||
+            next_props?.force_system !== this.props.force_system
         ) {
             if (next_props.force_system) {
                 update = Object.assign(
                     this.state,
                     recallTimeControlSettings(this.state.speed, next_props.force_system),
+                    {
+                        // if `value` and `force` are both asserted, with different `system`, then `force` wins,
+                        // and the previously saved settings for that `system` will be used instead of `value`
+                        ...(next_props.value && next_props.value.system === next_props.force_system
+                            ? next_props.value
+                            : {}),
+                    },
                 );
             } else if (next_props.value) {
                 update = { ...makeTimeControlParameters(next_props.value) };
