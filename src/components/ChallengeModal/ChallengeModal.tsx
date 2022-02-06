b7d117f85a24e886a714b717fe202bb1c4bcc435
GreenAsJade <greenasjade@gregories.net>
2022-02-06 21:12:16 +1030

    Allow rengo auto handicap option in ChallengeModal

--- src/components/ChallengeModal/ChallengeModal.tsx
+++ src/components/ChallengeModal/ChallengeModal.tsx
@@ -1144,7 +1144,6 @@ export class ChallengeModal extends Modal<Events, ChallengeModalProperties, any>
                                         className="challenge-dropdown form-control"
                                     >
                                         <option
-                                            disabled={challenge.game.rengo}
                                             value="-1"
                                             /*{disabled={!this.state.conf.handicap_enabled}}*/
                                         >
