a7856a02e061f474f47e7bed25a0dc0065fc4ebf
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-02-19 20:09:18 -0800

    Add PlayerDetails to rest_api types.

--- src/models/user.d.ts
+++ src/models/user.d.ts
@@ -55,4 +55,79 @@ declare namespace rest_api {
         email_validated: boolean | string; // VerifyEmail sets this to a Date string
         is_announcer: boolean;
     }
+
+    interface PlayerDetails {
+        user: {
+            id: number;
+            username: string;
+            professional: boolean;
+            ranking: number;
+            rating: number;
+            deviation: number;
+            ratings: {
+                version: number;
+                overall: RatingsConfig;
+            } & {
+                [game_type in
+                    | import("../lib/types").Size
+                    | import("../lib/types").Speed
+                    | `${import("../lib/types").Speed}-${import("../lib/types").Size}`]: RatingsConfig;
+            };
+            country: string; // country code
+            language: string;
+            name: any | null;
+            first_name: any | null;
+            last_name: any | null;
+            real_name_is_private: true;
+            about: string;
+            supporter: boolean;
+            ui_class_extra: null;
+            is_moderator: boolean;
+            is_superuser: boolean;
+            is_tournament_moderator: boolean;
+            is_bot: boolean;
+            timeout_provisional: boolean;
+            bot_ai: any | null;
+            bot_owner: any | null;
+            bot_apikey?: any;
+            website: string;
+            icon: string; // URL
+            registration_date: "2018-04-09T13:04:44.987830Z";
+            vacation_left: number;
+            on_vacation: boolean;
+        };
+        active_games: players.full.Game[];
+        ladders: Array<{
+            rank: number;
+            id: number;
+            name: string;
+        }>;
+        tournaments: Array<{
+            id: number;
+            name: string;
+        }>;
+        titles: any[];
+        trophies: Array<{
+            tournament_id: number;
+            tournament_name: string;
+            icon: string;
+            title: string;
+        }>;
+        groups: any[];
+        is_friend: boolean;
+        friend_request_sent: boolean;
+        friend_request_received: boolean;
+        vs: {
+            wins: number;
+            losses: number;
+            draws: number;
+            history: any[];
+        };
+        block: {
+            block_chat: boolean;
+            block_games: boolean;
+        };
+        achievements: any[];
+        ip?: string;
+    }
 }
