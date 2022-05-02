f8a074341e11ecbc386758ce19ad4acfd96d944d
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-05-01 23:25:58 -0700

    Fix ChatLine type

--- src/views/Game/GameChat.tsx
+++ src/views/Game/GameChat.tsx
@@ -44,14 +44,15 @@ interface GameChatProperties {
 }
 
 interface ChatLine {
-    chat_id: number;
+    chat_id: string;
     body: string | AnalysisComment | ReviewComment;
     date: number;
     move_number: number;
-    from: number;
-    moves: string;
+    from?: number;
+    moves?: string;
     channel: string;
     player_id: number;
+    username?: string;
 }
 
 interface GameChatLineProperties {
