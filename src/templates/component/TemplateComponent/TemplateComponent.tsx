e2791d789a10f4c083b2c2051b1c2c00be8d2e02
GreenAsJade <greenasjade@gregories.net>
2022-06-05 10:05:56 +0930

    Added a component template

---
+++ src/templates/component/TemplateComponent/TemplateComponent.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2012-2022  Online-Go.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as React from "react";
+
+import * as data from "data";
+
+import { pgettext } from "translate";
+
+export function TemplateComponent(): JSX.Element {
+    const [username, setUsername] = React.useState("");
+
+    const hangs_around = React.useRef("forever");
+
+    const user = data.get("config.user"); // user.anonymous will be set if they aren't logged in
+
+    React.useEffect(() => {
+        // Don't forget that side-effects go here, executed after rendering the return value
+        console.log("Rendered!");
+        hangs_around.current = "rendered";
+    });
+
+    if (username === "") {
+        setUsername("Mr ReRender :)");
+    }
+
+    /* render */
+    return (
+        <div className="TemplateComponent">
+            {(!user.anonymous || null) &&
+                pgettext(
+                    "This text is just in a template, it's not used in the site",
+                    "Consider using pgettext instead of '_', to make translators' life easier",
+                )}
+            {(user.anonymous || null) && <i className="fa fa-question" />}
+
+            {<span className="hangs-around">{hangs_around.current}</span>}
+        </div>
+    );
+}
