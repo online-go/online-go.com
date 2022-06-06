7e1ea1f6024e8cd8ad380f272dc8cb6494680897
Benjamin Jones <BenjaminPaikJones@gmail.com>
2022-06-06 07:09:45 -0700

    Fix displayWidth effect: OR -> AND

--- src/components/MiniGoban/MiniGoban.tsx
+++ src/components/MiniGoban/MiniGoban.tsx
@@ -235,7 +235,7 @@ export function MiniGoban(props: MiniGobanProps): JSX.Element {
 
     // Update displayWidth dynamically
     React.useEffect(() => {
-        if (!goban.current && props.displayWidth == null) {
+        if (!goban.current || props.displayWidth == null) {
             return;
         }
         goban.current.setSquareSizeBasedOnDisplayWidth(props.displayWidth);
