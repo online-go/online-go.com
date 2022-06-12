c3b194e85192ad59db6c8ffc866479f8ca8a008c
GreenAsJade <greenasjade@gregories.net>
2022-06-12 11:54:30 +0930

    Factored out Settings page components

---
+++ src/views/Settings/GeneralPreferences.tsx
@@ -0,0 +1,296 @@
+/*
+ * Copyright (C) 2012-2022  Online-Go.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as React from "react";
+import Select from "react-select";
+
+import * as data from "data";
+
+import { _ } from "translate";
+import { put } from "requests";
+import { errorAlerter, ignore, dev_site } from "misc";
+
+import { current_language, setCurrentLanguage, languages } from "translate";
+import { toast } from "toast";
+import { profanity_regex } from "profanity_filter";
+
+import * as preferences from "preferences";
+import { usePreference } from "preferences";
+
+import { Toggle } from "Toggle";
+
+import { SettingGroupProps, PreferenceLine, PreferenceDropdown } from "./Settings";
+
+export function GeneralPreferences(props: SettingGroupProps): JSX.Element {
+    const [profanity_filter, _setProfanityFilter]: [Array<string>, (x: Array<string>) => void] =
+        React.useState(Object.keys(preferences.get("profanity-filter")));
+
+    const [game_list_threshold, _setGameListThreshold]: [number, (x: number) => void] =
+        React.useState(preferences.get("game-list-threshold"));
+    const [_desktop_notifications, setDesktopNotifications] =
+        usePreference("desktop-notifications");
+    const [show_offline_friends, setShowOfflineFriends] = usePreference("show-offline-friends");
+    const [unicode_filter_usernames, setUnicodeFilterUsernames] = usePreference("unicode-filter");
+    const [translation_dialog_never_show, setTranslationDialogNeverShow] = usePreference(
+        "translation-dialog-never-show",
+    );
+    const [hide_ui_class, setHideUiClass]: [boolean, (x: boolean) => void] = React.useState(
+        props.state.hide_ui_class,
+    );
+    const [show_tournament_indicator, setShowTournamentIndicator] = usePreference(
+        "show-tournament-indicator",
+    );
+    const [hide_ranks, setHideRanks] = usePreference("hide-ranks");
+    const [rating_graph_always_use, setAlwaysUse] = usePreference("rating-graph-always-use");
+    const [rating_graph_plot_by_games, setPlotByGames] = usePreference(
+        "rating-graph-plot-by-games",
+    );
+    const [enable_v6, setEnableV6]: [boolean, (x: boolean) => void] = React.useState(
+        data.get("experiments.v6") === "enabled",
+    );
+
+    const user = data.get("user");
+    const desktop_notifications_enableable: boolean = typeof Notification !== "undefined";
+    const v6_experiment_enabled: boolean =
+        window.location.hostname !== "online-go.com" || user.is_moderator;
+
+    let desktop_notifications_enabled = false;
+    try {
+        desktop_notifications_enabled =
+            preferences.get("desktop-notifications") &&
+            (Notification as any).permission === "granted";
+    } catch (e) {
+        /* not all browsers support the Notification API */
+    }
+
+    function updateProfanityFilter(langs: { value: string; label: string }[]) {
+        if (!langs) {
+            langs = [];
+        }
+
+        const new_profanity_settings: { [cc: string]: true } = {};
+
+        langs.forEach((lang) => {
+            new_profanity_settings[lang.value] = true;
+        });
+
+        preferences.set("profanity-filter", new_profanity_settings);
+        _setProfanityFilter(langs.map((lang) => lang.value));
+    }
+
+    function updateGameListThreshold(ev: React.ChangeEvent<HTMLInputElement>) {
+        let threshold = parseInt(ev.target.value);
+        if (!isNaN(threshold)) {
+            threshold = Math.min(300, Math.max(0, threshold));
+            preferences.set("game-list-threshold", threshold);
+            _setGameListThreshold(threshold);
+        } else {
+            _setGameListThreshold(ev.target.value as any);
+        }
+    }
+
+    function updateDesktopNotifications(enabled) {
+        if (!enabled) {
+            //this.setState({'desktop_notifications_enabled': false});
+        }
+
+        try {
+            setDesktopNotifications(enabled);
+
+            if (enabled) {
+                if ((Notification as any).permission === "denied") {
+                    //this.setState({'desktop_notifications_enabled': false});
+                    toast(
+                        <div>
+                            {_(
+                                "You have previously denied desktop notifications on OGS, you will need to go into your browser settings and change your decision there to enable them.",
+                            )}
+                        </div>,
+                    );
+                }
+
+                if ((Notification as any).permission === "granted") {
+                    //this.setState({'desktop_notifications_enabled': true});
+                }
+
+                if ((Notification as any).permission === "default") {
+                    const onRequestResult = (perm) => {
+                        if (perm === "granted") {
+                            //this.setState({'desktop_notifications_enabled': true});
+                            console.log("granted notification permission");
+                        } else {
+                            //this.setState({'desktop_notifications_enabled': false});
+                            toast(
+                                <div>
+                                    {_(
+                                        "You have previously denied desktop notifications on OGS, you will need to go into your browser settings and change your decision there to enable them.",
+                                    )}
+                                </div>,
+                            );
+                        }
+                    };
+
+                    try {
+                        Notification.requestPermission().then(onRequestResult).catch(ignore);
+                    } catch (e) {
+                        /* deprecated usage, but only way supported on safari currently */
+                        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+                        Notification.requestPermission(onRequestResult);
+                    }
+                }
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    function updateHideUIClass(checked) {
+        setHideUiClass(!checked);
+        put(`me/settings`, {
+            site_preferences: {
+                hide_ui_class: !checked,
+            },
+        }).catch(errorAlerter);
+    }
+
+    const show_debug_language = dev_site();
+    const language_options = Object.entries(languages)
+        .filter((lang_entry) => show_debug_language || lang_entry[0] !== "debug")
+        .map((lang_entry) => ({
+            value: lang_entry[0],
+            label: lang_entry[1],
+        }));
+
+    function setLanguage(language_code: string) {
+        preferences.set("language", language_code);
+        setCurrentLanguage(language_code);
+        window.location.reload();
+    }
+
+    return (
+        <div>
+            <PreferenceLine title={_("Language")}>
+                <PreferenceDropdown
+                    value={current_language}
+                    options={language_options}
+                    onChange={setLanguage}
+                />
+            </PreferenceLine>
+
+            <PreferenceLine title={_("Profanity filter")}>
+                <Select
+                    className="ProfanityDropdown"
+                    classNamePrefix="ogs-react-select"
+                    defaultValue={language_options.filter(
+                        (lang) => profanity_filter.indexOf(lang.value) >= 0,
+                    )}
+                    options={language_options.filter((lang) => lang.value in profanity_regex)}
+                    onChange={updateProfanityFilter}
+                    isMulti
+                />
+            </PreferenceLine>
+
+            <PreferenceLine
+                title={_("Game thumbnail list threshold")}
+                description={_("Set to 0 to always show list.")}
+            >
+                <input
+                    value={game_list_threshold}
+                    onChange={updateGameListThreshold}
+                    type="number"
+                    min="0"
+                    max="300"
+                    step="1"
+                />
+            </PreferenceLine>
+
+            <PreferenceLine title={_("Desktop notifications")}>
+                <Toggle
+                    checked={desktop_notifications_enabled}
+                    onChange={updateDesktopNotifications}
+                    disabled={!desktop_notifications_enableable}
+                />
+
+                {!desktop_notifications_enableable && (
+                    <div>
+                        <i>{_("Desktop notifications are not supported by your browser")}</i>
+                    </div>
+                )}
+            </PreferenceLine>
+
+            <PreferenceLine title={_("Show offline friends on list")}>
+                <Toggle checked={show_offline_friends} onChange={setShowOfflineFriends} />
+            </PreferenceLine>
+
+            <PreferenceLine title={_("Hide special unicode symbols in usernames")}>
+                <Toggle checked={unicode_filter_usernames} onChange={setUnicodeFilterUsernames} />
+            </PreferenceLine>
+
+            <PreferenceLine
+                title={_('Never show the "needs translation" message on the home page')}
+            >
+                <Toggle
+                    checked={translation_dialog_never_show}
+                    onChange={setTranslationDialogNeverShow}
+                />
+            </PreferenceLine>
+
+            {(user.supporter || user.is_moderator || null) && (
+                <PreferenceLine title={_("Golden supporter name")}>
+                    <Toggle
+                        checked={!hide_ui_class}
+                        onChange={updateHideUIClass}
+                        id="hide_ui_class"
+                    />
+                </PreferenceLine>
+            )}
+
+            <PreferenceLine title={_("Show tournament indicator")}>
+                <Toggle checked={show_tournament_indicator} onChange={setShowTournamentIndicator} />
+            </PreferenceLine>
+
+            <PreferenceLine title={_("Hide ranks and ratings")}>
+                <Toggle checked={hide_ranks} onChange={setHideRanks} />
+            </PreferenceLine>
+
+            {v6_experiment_enabled && (
+                <PreferenceLine title={"Enable experimental interface changes"}>
+                    <Toggle
+                        checked={enable_v6}
+                        onChange={(tf) => {
+                            data.set("experiments.v6", tf ? "enabled" : undefined);
+                            setEnableV6(tf);
+                        }}
+                    />
+                </PreferenceLine>
+            )}
+
+            <PreferenceLine title={_("Plot rating graph by")}>
+                <span>{_("Ask me")}</span>
+                <Toggle checked={rating_graph_always_use} onChange={setAlwaysUse} />
+                <span>{_("Always use:")}</span>
+                <span>{_("time")}</span>
+                <Toggle
+                    checked={rating_graph_plot_by_games}
+                    onChange={setPlotByGames}
+                    disabled={!preferences.get("rating-graph-always-use")}
+                />
+                <span>{_("games")}</span>
+            </PreferenceLine>
+        </div>
+    );
+}
---
+++ src/views/Settings/LinkPreferences.tsx
@@ -0,0 +1,275 @@
+/*
+ * Copyright (C) 2012-2022  Online-Go.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as React from "react";
+
+import { _, pgettext } from "translate";
+
+import * as data from "data";
+import { put } from "requests";
+import { errorAlerter, Timeout } from "misc";
+
+import { Toggle } from "Toggle";
+
+import { SettingGroupProps, PreferenceLine } from "./Settings";
+
+import { IAssociation, associations } from "associations";
+import { allRanks, IRankInfo } from "rank_utils";
+
+let update_link_preferences_debounce: Timeout;
+
+export function LinkPreferences(props: SettingGroupProps): JSX.Element {
+    const link = props.state.self_reported_account_linkages || {};
+
+    function set(key: string): (value: any) => void {
+        return (value: any) => {
+            if (typeof value === "object") {
+                value = value.target.value; // event
+            }
+
+            link[key] = value;
+            props.updateSelfReportedAccountLinkages(link);
+            if (update_link_preferences_debounce) {
+                clearTimeout(update_link_preferences_debounce);
+            }
+            update_link_preferences_debounce = setTimeout(() => {
+                clearTimeout(update_link_preferences_debounce);
+                update_link_preferences_debounce = undefined;
+                put("me/settings", {
+                    self_reported_account_linkages: props.state.self_reported_account_linkages,
+                })
+                    .then((res) => console.log(res))
+                    .catch(errorAlerter);
+            }, 500);
+        };
+    }
+
+    return (
+        <div id="LinkPreferences">
+            <div className="LinkPreferencesDescription">
+                {_(
+                    "Here you can list other places you play and have a rating or a rank. You can choose to publically display this information or not. Providing this even if you don't want it publically known helps us tune our ranking algorithm and provide guidance on converting ranks between servers and organizations for the community, so the information is important and greatly appreciated.",
+                )}
+            </div>
+
+            <PreferenceLine title={_("Show this information on your profile page")}>
+                <Toggle checked={!link.hidden} onChange={(tf) => set("hidden")(!tf)} />
+            </PreferenceLine>
+
+            <PreferenceLine title={_("Only show ranks, not ids and usernames")}>
+                <Toggle
+                    disabled={link.hidden}
+                    checked={link.hidden_ids && !link.hidden}
+                    onChange={(tf) => set("hidden_ids")(tf)}
+                />
+            </PreferenceLine>
+
+            <h2>{_("Associations")}</h2>
+
+            <PreferenceLine title={<AssociationSelect value={link.org1} onChange={set("org1")} />}>
+                <input
+                    type="text"
+                    placeholder={pgettext("Go association Identifier or PIN number", "ID or PIN")}
+                    value={link.org1_id || ""}
+                    onChange={set("org1_id")}
+                />
+                <RankSelect value={link.org1_rank} onChange={set("org1_rank")} />
+            </PreferenceLine>
+
+            <PreferenceLine title={<AssociationSelect value={link.org2} onChange={set("org2")} />}>
+                <input
+                    type="text"
+                    placeholder={pgettext("Go association Identifier or PIN number", "ID or PIN")}
+                    value={link.org2_id || ""}
+                    onChange={set("org2_id")}
+                />
+                <RankSelect value={link.org2_rank} onChange={set("org2_rank")} />
+            </PreferenceLine>
+
+            <PreferenceLine title={<AssociationSelect value={link.org3} onChange={set("org3")} />}>
+                <input
+                    type="text"
+                    placeholder={pgettext("Go association Identifier or PIN number", "ID or PIN")}
+                    value={link.org3_id || ""}
+                    onChange={set("org3_id")}
+                />
+                <RankSelect value={link.org3_rank} onChange={set("org3_rank")} />
+            </PreferenceLine>
+
+            <h2>{_("Servers")}</h2>
+            <PreferenceLine title={_("KGS")}>
+                <input
+                    type="text"
+                    placeholder={_("Username")}
+                    value={link.kgs_username || ""}
+                    onChange={set("kgs_username")}
+                />
+                <RankSelect value={link.kgs_rank} onChange={set("kgs_rank")} />
+            </PreferenceLine>
+            <PreferenceLine title={_("IGS / PandaNet")}>
+                <input
+                    type="text"
+                    placeholder={_("Username")}
+                    value={link.igs_username || ""}
+                    onChange={set("igs_username")}
+                />
+                <RankSelect value={link.igs_rank} onChange={set("igs_rank")} />
+            </PreferenceLine>
+            <PreferenceLine title={_("DGS")}>
+                <input
+                    type="text"
+                    placeholder={_("Username")}
+                    value={link.dgs_username || ""}
+                    onChange={set("dgs_username")}
+                />
+                <RankSelect value={link.dgs_rank} onChange={set("dgs_rank")} />
+            </PreferenceLine>
+            <PreferenceLine title={_("Little Golem")}>
+                <input
+                    type="text"
+                    placeholder={_("Username")}
+                    value={link.golem_username || ""}
+                    onChange={set("golem_username")}
+                />
+                <RankSelect value={link.golem_rank} onChange={set("golem_rank")} />
+            </PreferenceLine>
+            <PreferenceLine title={_("WBaduk")}>
+                <input
+                    type="text"
+                    placeholder={_("Username")}
+                    value={link.wbaduk_username || ""}
+                    onChange={set("wbaduk_username")}
+                />
+                <RankSelect value={link.wbaduk_rank} onChange={set("wbaduk_rank")} />
+            </PreferenceLine>
+            <PreferenceLine title={_("Tygem")}>
+                <input
+                    type="text"
+                    placeholder={_("Username")}
+                    value={link.tygem_username || ""}
+                    onChange={set("tygem_username")}
+                />
+                <RankSelect value={link.tygem_rank} onChange={set("tygem_rank")} />
+            </PreferenceLine>
+            <PreferenceLine title={_("Fox")}>
+                <input
+                    type="text"
+                    placeholder={_("Username")}
+                    value={link.fox_username || ""}
+                    onChange={set("fox_username")}
+                />
+                <RankSelect value={link.fox_rank} onChange={set("fox_rank")} />
+            </PreferenceLine>
+            <PreferenceLine title={_("Yike Weiqi")}>
+                <input
+                    type="text"
+                    placeholder={_("Username")}
+                    value={link.yike_username || ""}
+                    onChange={set("yike_username")}
+                />
+                <RankSelect value={link.yike_rank} onChange={set("yike_rank")} />
+            </PreferenceLine>
+            <PreferenceLine title={_("GoQuest")}>
+                <input
+                    type="text"
+                    placeholder={_("Username")}
+                    value={link.goquest_username || ""}
+                    onChange={set("goquest_username")}
+                />
+                <RankSelect value={link.goquest_rank} onChange={set("goquest_rank")} />
+            </PreferenceLine>
+        </div>
+    );
+}
+
+const rank_select_ranks = allRanks();
+function RankSelect({
+    value,
+    onChange,
+}: {
+    value: number;
+    onChange: (value: number) => void;
+}): JSX.Element {
+    return (
+        <select
+            className="RankSelect"
+            value={value}
+            onChange={(ev) => onChange(parseInt(ev.target.value))}
+        >
+            <option value={-999}>{_("-- Select Rank --")}</option>
+            {rank_select_ranks.map((rank: IRankInfo) => (
+                <option key={rank.rank} value={rank.rank}>
+                    {rank.label}
+                </option>
+            ))}
+        </select>
+    );
+}
+
+function AssociationSelect({
+    value,
+    onChange,
+}: {
+    value: string;
+    onChange: (value: string) => void;
+}): JSX.Element {
+    let user_countries = [];
+    try {
+        if (data.get("user").country) {
+            /* If there's an association for the user's country, we put it at the top of the list */
+            if (associations.filter((a) => a.country === data.get("user").country).length > 0) {
+                user_countries.push(data.get("user").country);
+            }
+        }
+    } catch (err) {
+        // pass
+    }
+
+    if (user_countries.length === 0 || user_countries[0] === "un") {
+        /* Couldn't figure out a best assocation to put up top? Put the most common ones up on top */
+        user_countries = ["us", "eu", "jp", "cn", "kr"];
+    }
+
+    associations.sort((a: IAssociation, b: IAssociation) => {
+        if (user_countries.indexOf(a.country) >= 0) {
+            if (user_countries.indexOf(b.country) >= 0) {
+                return a.name < b.name ? -1 : 1;
+            }
+            return -1;
+        }
+        if (user_countries.indexOf(b.country) >= 0) {
+            return 1;
+        }
+
+        return a.country < b.country ? -1 : 1;
+    });
+
+    return (
+        <select
+            className="AssociationSelect"
+            value={value}
+            onChange={(ev) => onChange(ev.target.value)}
+        >
+            <option value={""}>{_("-- Select Association --")}</option>
+            {associations.map((association: IAssociation) => (
+                <option key={association.country} value={association.country}>
+                    {association.name}
+                </option>
+            ))}
+        </select>
+    );
+}
