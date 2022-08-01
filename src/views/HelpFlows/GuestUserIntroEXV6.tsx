8bc043d4702e154450678e0b4439062e46b34ee3
GreenAsJade <greenasjade@gregories.net>
2022-08-01 16:45:13 +0930

    Make separate files for each help flow, support old Nav in addition to new.

---
+++ src/views/HelpFlows/GuestUserIntroEXV6.tsx
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2012-2022  Online-Go.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+
+import { HelpFlow, HelpItem } from "react-dynamic-help";
+
+import { _ } from "translate";
+
+/**
+ * A help flow intended for guests who arrived on a Challenge Link
+ * (targets components in the EXV6 flow)
+ */
+
+export function GuestUserIntroEXV6(): JSX.Element {
+    return (
+        <HelpFlow id="guest-user-intro-exv6" showInitially={false} debug={false}>
+            <HelpItem target="toggle-right-nav" position={"bottom-left"}>
+                <div>{_("To set your password, click here")} </div>
+            </HelpItem>
+
+            <HelpItem target="settings-nav-link" position={"center-left"} anchor={"top-right"}>
+                <div>{_("To set your password, click here")} </div>
+            </HelpItem>
+
+            <HelpItem
+                target="account-settings-button"
+                position={"center-left"}
+                anchor={"top-right"}
+            >
+                <div>{_("To set your password, click here")} </div>
+            </HelpItem>
+
+            <HelpItem target="password-entry" position={"center-right"}>
+                <div>{_("You can enter your new password here.")} </div>
+            </HelpItem>
+
+            <HelpItem target="profile-edit-link">
+                <div>{_("You can also change your username, here.")} </div>
+            </HelpItem>
+
+            <HelpItem
+                target="profile-edit-page"
+                position="center-left"
+                id="help-for-profile-edit-page"
+            >
+                <div>{_("In here you can change your profile appearance, then click 'Save'.")}</div>
+            </HelpItem>
+        </HelpFlow>
+    );
+}
