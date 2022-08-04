1cf4a18217c6fe5a6de844436e6666d2f35859fd
Akita Noek <anoek@online-go.com>
2022-08-04 11:35:33 -0600

    Hookified ladders list

--- src/views/LadderList/LadderList.tsx
+++ src/views/LadderList/LadderList.tsx
@@ -16,107 +16,93 @@
  */
 
 import * as React from "react";
-import * as data from "data";
 import { _, interpolate } from "translate";
 import { Link } from "react-router-dom";
 import { post, get } from "requests";
 import { LadderComponent } from "LadderComponent";
 import { Card } from "material";
 import { errorAlerter } from "misc";
+import { useUser } from "hooks";
 
 /* Ensure these get picked up in our translations */
 _("Site 19x19 Ladder");
 _("Site 13x13 Ladder");
 _("Site 9x9 Ladder");
 
-interface LadderListState {
-    ladders: Array<{
-        id: number;
-        board_size: number;
-        name: string;
-        player_rank: number;
-        size: number;
-    }>;
+interface LadderEntry {
+    id: number;
+    board_size: number;
+    name: string;
+    player_rank: number;
+    size: number;
 }
 
-export class LadderList extends React.PureComponent<{}, LadderListState> {
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            ladders: [],
-        };
-    }
+export function LadderList(): JSX.Element {
+    const [ladders, setLadders] = React.useState<Array<LadderEntry>>([]);
+    const user = useUser();
 
-    componentDidMount() {
+    React.useEffect(() => {
         window.document.title = "Ladders";
-        this.resolve();
-    }
+        resolve();
+    }, []);
 
-    resolve() {
+    function resolve() {
         get("ladders")
             .then((res) => {
-                this.setState({ ladders: res.results });
+                setLadders(res.results);
             })
             .catch(errorAlerter);
     }
 
-    join(ladder_id: number) {
+    const join = (ladder_id: number) => {
         post("ladders/%%/players", ladder_id, {})
             .then(() => {
                 this.resolve();
             })
             .catch(errorAlerter);
-    }
+    };
 
-    render() {
-        const user = data.get("user");
-
-        return (
-            <div className="page-width">
-                <div className="page-nav">
-                    <h2 style={{ marginLeft: "1rem" }}>
-                        <i className="fa fa-list-ol"></i> {_("Ladders")}
-                    </h2>
-                    <div>
-                        {this.state.ladders.map((ladder, idx) => (
-                            <Link key={idx} to={`/ladder/${ladder.id}`}>
-                                {_(ladder.board_size + "x" + ladder.board_size)}
-                            </Link>
-                        ))}
-                    </div>
-                </div>
-                <div className="LadderList">
-                    {this.state.ladders.map((ladder, idx) => (
-                        <Card key={idx}>
-                            <h2>{_(ladder.name)}</h2>
-                            {(ladder.player_rank < 0 || null) && (
-                                <button
-                                    className="primary sm"
-                                    disabled={user.anonymous}
-                                    onClick={this.join.bind(this, ladder.id)}
-                                >
-                                    {_("Join")}
-                                </button>
-                            )}
-                            <Link className="btn primary sm" to={`/ladder/${ladder.id}`}>
-                                {
-                                    _(
-                                        "Full View",
-                                    ) /* translators: View details of the selected ladder */
-                                }
-                            </Link>
-
-                            <h4>
-                                {interpolate(_("{{ladder_size}} players"), {
-                                    ladder_size: ladder.size,
-                                })}
-                            </h4>
-
-                            <LadderComponent ladderId={ladder.id} />
-                        </Card>
+    return (
+        <div className="page-width">
+            <div className="page-nav">
+                <h2 style={{ marginLeft: "1rem" }}>
+                    <i className="fa fa-list-ol"></i> {_("Ladders")}
+                </h2>
+                <div>
+                    {ladders.map((ladder, idx) => (
+                        <Link key={idx} to={`/ladder/${ladder.id}`}>
+                            {_(ladder.board_size + "x" + ladder.board_size)}
+                        </Link>
                     ))}
                 </div>
             </div>
-        );
-    }
+            <div className="LadderList">
+                {ladders.map((ladder, idx) => (
+                    <Card key={idx}>
+                        <h2>{_(ladder.name)}</h2>
+                        {(ladder.player_rank < 0 || null) && (
+                            <button
+                                className="primary sm"
+                                disabled={user.anonymous}
+                                onClick={() => join(ladder.id)}
+                            >
+                                {_("Join")}
+                            </button>
+                        )}
+                        <Link className="btn primary sm" to={`/ladder/${ladder.id}`}>
+                            {_("Full View") /* translators: View details of the selected ladder */}
+                        </Link>
+
+                        <h4>
+                            {interpolate(_("{{ladder_size}} players"), {
+                                ladder_size: ladder.size,
+                            })}
+                        </h4>
+
+                        <LadderComponent ladderId={ladder.id} />
+                    </Card>
+                ))}
+            </div>
+        </div>
+    );
 }
