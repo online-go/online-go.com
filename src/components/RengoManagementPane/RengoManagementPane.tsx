752d19a02ae4ab192317f936657dee5d37befc72
GreenAsJade <greenasjade@gregories.net>
2022-08-27 07:44:30 +0930

    Put back the `this `s

--- src/components/RengoManagementPane/RengoManagementPane.tsx
+++ src/components/RengoManagementPane/RengoManagementPane.tsx
@@ -1,18 +1,18 @@
 /*
  * Copyright (C) 2012-2017  Online-Go.com
  *
- * program is free software: you can redistribute it and/or modify
+ * THis program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * program is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import * as React from "react";
@@ -32,7 +32,7 @@ interface RengoManagementPaneProperties {
     children: React.ReactNode; // intended for team management pane, which receives different props, direct from our parent
 }
 
-/** Pane is designed to manage a challenge identified by `id`,
+/** This Pane is designed to manage a challenge identified by `id`,
  * picked out of a supplied list of challenges
  * */
 
