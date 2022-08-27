eab55c0dd704717c70e66858654e2dc65de9ae81
GreenAsJade <greenasjade@gregories.net>
2022-08-28 09:17:53 +0930

    Clean up comment, fix potential (but not curent) bug (rdh doesn't use this value anyhow, but it might one day!)

--- src/main.tsx
+++ src/main.tsx
@@ -319,7 +319,7 @@ const helpPopupDictionary: HelpPopupDictionary = {
 
 // Make help system use our server-based storage, to achieve logged-in-user-specific help state.
 
-// Note: possibly data.ts should take care of "whether REMOTE_OVERRIDES_LOCAL is ready"
+// Prevent writing back rdhState till remote data is loaded
 //  wait till remote data is loaded
 let storage_loaded = false;
 
@@ -339,7 +339,7 @@ const dynamicHelpStorage: DynamicHelp.DynamicHelpStorageAPI = {
             return data.set("rdh-system-state", rdhState, data.Replication.REMOTE_OVERWRITES_LOCAL);
         } else {
             debugDynamicHelp && console.log("NOT writing rdhState");
-            return "{}";
+            return rdhState;
         }
     },
     getState: (defaultValue?: string) => {
