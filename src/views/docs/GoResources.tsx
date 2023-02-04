online-go/online-go.com

commit 2ccee72a3ce88bd0d1321687306b4983dacf00b1

Updated copyright headers to drop the year as we deamed it unecessary

src/views/docs/GoResources.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2022  Online-Go.com
+ * Copyright (C)  Online-Go.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -143,7 +143,7 @@ export const GoResources = () => {
                                 ]}
                                 editions={[
                                     {
-                                        title: "2022 Edition (pdf)",
+                                        title: " Edition (pdf)",
                                         country: en,
                                         href: "https://cdn.online-go.com/81_little_lions/81_little_lions_an_introduction_to_the_9x9_board_for_advanced_beginners_2022.pdf",
                                     },

commit b2c60a011fc0d3735bd94325d95e61db450f7675

Fix book title
Fix book title broken by commit 2ccee72a3ce88bd0d1321687306b4983dacf00b1

src/views/docs/GoResources.tsx
@@ -143,7 +143,7 @@ export const GoResources = () => {
                                 ]}
                                 editions={[
                                     {
-                                        title: " Edition (pdf)",
+                                        title: "2022 Edition (pdf)",
                                         country: en,
                                         href: "https://cdn.online-go.com/81_little_lions/81_little_lions_an_introduction_to_the_9x9_board_for_advanced_beginners_2022.pdf",
                                     },