online-go/online-go.com

commit 2ccee72a3ce88bd0d1321687306b4983dacf00b1

Updated copyright headers to drop the year as we deamed it unecessary

src/components/Achievements/AchievementList.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2022  Online-Go.com
+ * Copyright (C)  Online-Go.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -45,7 +45,7 @@ function render_achievement_entry(entry: AchievementEntry): JSX.Element {
             break;
         case "wsc2022":
             title = "Western Server Challenge Contender";
-            description = "Played 100 or more games during the 2022 Western Server Challenge";
+            description = "Played 100 or more games during the  Western Server Challenge";
             break;
     }
 

commit ad1cb651abd706a6c2d696455dfd4dcbe28cadf0

Fix missing year on WSC achievement
Fix missing year on WSC achievement broken by commit 2ccee72a3ce88bd0d1321687306b4983dacf00b1

src/components/Achievements/AchievementList.tsx
@@ -45,7 +45,7 @@ function render_achievement_entry(entry: AchievementEntry): JSX.Element {
             break;
         case "wsc2022":
             title = "Western Server Challenge Contender";
-            description = "Played 100 or more games during the  Western Server Challenge";
+            description = "Played 100 or more games during the 2022 Western Server Challenge";
             break;
     }
 