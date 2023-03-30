7a64fe216cfb3dc4fd6a1806a03c77f82d912b52
Akita Noek <anoek@online-go.com>
2023-03-30 05:14:11 -0600

    Set test user for GameDock test

--- src/views/Game/GameDock.test.tsx
+++ src/views/Game/GameDock.test.tsx
@@ -4,6 +4,34 @@ import { GameDock } from "./GameDock";
 import { Goban } from "goban";
 import { GobanContext } from "./goban_context";
 import { BrowserRouter as Router } from "react-router-dom";
+import * as data from "data";
+
+const TEST_USER = {
+    anonymous: false,
+    id: 123,
+    username: "test_user",
+    registration_date: "2022-05-10 11:03:24.299562+00:00",
+    ratings: {
+        version: 5,
+        overall: { rating: 1500, deviation: 350, volatility: 0.06 },
+    },
+    country: "un",
+    professional: false,
+    ranking: 23,
+    provisional: 0,
+    can_create_tournaments: true,
+    is_moderator: false,
+    is_superuser: false,
+    is_tournament_moderator: false,
+    supporter: true,
+    supporter_level: 4,
+    tournament_admin: false,
+    ui_class: "",
+    icon: "https://secure.gravatar.com/avatar/8d809ecc50408afc399a4cb7c8fd4510?s=32&d=retro",
+    email: "",
+    email_validated: false,
+    is_announcer: false,
+} as const;
 
 const BASE_PROPS = {
     annulled: false,
@@ -25,6 +53,7 @@ const BASE_PROPS = {
 };
 
 test("providing both Game ID and Review ID cause SGF buttons to link to review SGFs", () => {
+    data.set("user", TEST_USER);
     const goban = new Goban({ game_id: 123456, review_id: 123 });
 
     render(
