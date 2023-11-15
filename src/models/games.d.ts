9d5c18fab4ed0aaea314b8043d5556f415c2f9ae
Matt Etress <mattetress@gmail.com>
2023-11-14 23:52:08 -0600

    Add bot detection results to game interface

--- src/models/games.d.ts
+++ src/models/games.d.ts
@@ -71,6 +71,7 @@ declare namespace rest_api {
      * One element of `results` from `player/%player_id%/games`
      */
     interface Game extends GameBase {
+        bot_detection_results: Record<string, any>;
         related: {
             detail: string; // route to full game info
         };
@@ -228,6 +229,7 @@ declare namespace rest_api {
      * The response from `games/%game_id%`
      */
     interface GameDetails extends GameBase {
+        bot_detection_results: null | Record<string, any>;
         related: {
             reviews: string; // route to reviews
         };
