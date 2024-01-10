43c41a08f46f07a1944b4c8f1934cb8aef9ffcf2
Akita Noek <anoek@online-go.com>
2024-01-10 09:40:57 -0700

    Filter out some errors that would be sent to sentry that come from user addons and the like

--- src/main.tsx
+++ src/main.tsx
@@ -69,6 +69,34 @@ try {
                 console: false,
             }),
         ],
+        beforeSend(event, hint) {
+            try {
+                const error: any = hint.originalException;
+                /* Several users have weird addons and extensions that cause errors
+                 * that have nothing to do with OGS. This code filters some of these
+                 * out so they don't get reported to Sentry. */
+
+                const strings_to_ignore = [
+                    "coinbase",
+                    "hideMyLocation",
+                    "userAgent",
+                    "zaloJSV2", // cspell:disable-line
+                    "evaluating 'a.L'",
+                    "document.querySelector(\"[title='Kaya']\").style",
+                ];
+
+                if (
+                    error &&
+                    error.message &&
+                    strings_to_ignore.some((str) => error.message.indexOf(str) >= 0)
+                ) {
+                    return null;
+                }
+            } catch (e) {
+                // ignore
+            }
+            return event;
+        },
     });
 
     Sentry.setTag("version", ogs_version || "dev");
