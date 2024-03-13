03a198ffa22cba08627a00baa359b0457466035e
Matt Etress <mattetress@gmail.com>
2024-03-13 16:21:44 -0500

    Add PrizeBatch component

---
+++ src/views/Prizes/PrizeBatch.tsx
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C)  Online-Go.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { get, post } from "requests";
+
+interface PrizeBatch {
+    id: string;
+    created_at: string;
+    expiration_date: string;
+    created_by: number;
+    notes: string;
+    codes: any[];
+}
+
+interface PrizeBatchParams {
+    id: string;
+}
+
+export const PrizeBatch: React.FC = () => {
+    const params = useParams<keyof PrizeBatchParams>();
+    const [batch, setBatch] = useState<PrizeBatch>();
+    const [qty, setQty] = useState(1);
+    const [duration, setDuration] = useState(30);
+    const [level, setLevel] = useState("aji");
+
+    useEffect(() => {
+        const url = "prizes/batches/" + params.id;
+        get(url)
+            .then((data: PrizeBatch) => setBatch(data))
+            .catch((error) => console.error("Error fetching prize batches:", error));
+    }, []);
+
+    const handleQtyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQty(parseInt(event.target.value));
+    };
+
+    const handleDurationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDuration(parseInt(event.target.value));
+    };
+
+    const handleLevelChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setLevel(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = {
+            qty: qty,
+            duration: duration,
+            level: level,
+        };
+        post("prizes/batches/" + params.id, data)
+            .then((res) => {
+                console.log("RESPONSE: ", res);
+                setBatch(res);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
+
+    return (
+        <div className="prize-batch">
+            <div className="batch-info">
+                Batch ID: {batch?.id}
+                <br />
+                Created By: {batch?.created_by}
+                <br />
+                Expires: {batch?.expiration_date}
+            </div>
+            <div className="codes">
+                <h3>Codes:</h3>
+                {batch?.codes.map((code, i) => {
+                    return (
+                        <li key={i}>
+                            {code.code} - {code.redeemed_at ? code.redeemed_at : "N/A"}
+                        </li>
+                    );
+                })}
+            </div>
+            <div className="add-codes-form">
+                <h3>Add Codes</h3>
+                <form onSubmit={handleSubmit}>
+                    <label>Quantity: </label>
+                    <input type="number" value={qty} onChange={handleQtyChange} />
+                    <br />
+                    <label>Duration: </label>
+                    <input type="number" value={duration} onChange={handleDurationChange} /> days
+                    <br />
+                    <label>Level: </label>
+                    <select value={level} onChange={handleLevelChange}>
+                        <option value="aji">Aji</option>
+                        <option value="hane">Hane</option>
+                        <option value="tenuki">Tenuki</option>
+                        <option value="meijin">Meijin</option>
+                    </select>
+                    <input type="submit" value="Add Codes" />
+                </form>
+            </div>
+        </div>
+    );
+};
--- src/views/Prizes/index.ts
+++ src/views/Prizes/index.ts
@@ -16,3 +16,4 @@
  */
 
 export * from "./PrizeBatchList";
+export * from "./PrizeBatch";
