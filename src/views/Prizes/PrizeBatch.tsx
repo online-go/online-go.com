9ff8426bd5641c243ecb0e8cb39cdbbcecb1a5cb
Matt Etress <mattetress@gmail.com>
2024-03-21 09:13:54 -0500

    Use black and white logo on vouchers

--- src/views/Prizes/PrizeBatch.tsx
+++ src/views/Prizes/PrizeBatch.tsx
@@ -99,7 +99,7 @@ export const PrizeBatch: React.FC = () => {
                 html += `
                     <div class="voucher">
                         <div class="header">
-                            <img src="/ogs_light.svg" alt="OGS Logo" class="logo" />
+                            <img src="/ogs_bw.svg" alt="OGS Logo" class="logo" />
                             <h2>Prize Voucher</h2>
                         </div>
                         <div class="content">
