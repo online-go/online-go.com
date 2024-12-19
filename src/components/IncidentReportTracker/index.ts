4fa672342a5cdf19904ad909404374534e4550b9
GreenAsJade <github@gregories.net>
2024-12-19 21:04:22 +1030

    Refactor IncidentReportTracker so that the IncidentReportList can be used elsewhere in order to be able to show CMs their reports.

--- src/components/IncidentReportTracker/index.ts
+++ src/components/IncidentReportTracker/index.ts
@@ -15,4 +15,5 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export * from "./IncidentReportTracker";
+export * from "./IncidentReportList";
+export * from "./IncidentReportIndicator";
