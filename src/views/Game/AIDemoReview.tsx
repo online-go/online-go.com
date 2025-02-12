8c50bbafcf5025303b9c75029b01d82d98a04070
Akita Noek <anoek@online-go.com>
2025-02-12 14:25:54 -0700

    Simplify our player to move logic in demo board analysis requests by only sending the prior 2 moves instead of 3

--- src/views/Game/AIDemoReview.tsx
+++ src/views/Game/AIDemoReview.tsx
@@ -190,16 +190,18 @@ export function AIDemoReview({
 
                 const moves: JGOFMove[] = [];
 
-                /* We go back up to 3 moves to get a starting board state, and then send that board
+                /* We go back up to 2 moves to get a starting board state, and then send that board
                  * state along with the moves to get to our current move. This ensures we can
                  * handle really long variations like some folks like to do, and it also ensures
                  * the AI doesn't suggest obvious board repetitions. */
-                for (let i = 0; i < 3; i++) {
-                    if (move.parent) {
-                        moves.unshift(move.toJGOFMove());
-                        move = move.parent;
-                    } else {
-                        break;
+                if (move.move_number > 2) {
+                    for (let i = 0; i < 2; i++) {
+                        if (move.parent) {
+                            moves.unshift(move.toJGOFMove());
+                            move = move.parent;
+                        } else {
+                            break;
+                        }
                     }
                 }
 
