0d24569d4d6f6187838c686a6fc7f3ee05657471
GreenAsJade <github@gregories.net>
2025-03-13 17:24:20 +1030

    Node.js 23.x support (workaround broken flag)
    https://github.com/microsoft/playwright/issues/34263

---
+++ scripts/run-playwright.js
@@ -0,0 +1,15 @@
+import { spawnSync } from "child_process";
+
+const isNode23 = process.version.startsWith("v23");
+const baseCommand = "node_modules/playwright/cli.js";
+const node23workaround = "--no-experimental-strip-types";
+
+function runPlaywright(args = []) {
+    const nodeArgs = isNode23 ? [node23workaround] : [];
+    spawnSync("node", [...nodeArgs, baseCommand, "test", ...args], {
+        shell: true,
+        stdio: "inherit",
+    });
+}
+
+runPlaywright(process.argv.slice(2));
