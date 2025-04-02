5ce154559a602561b73746d2fba4de493f81119b
GreenAsJade <github@gregories.net>
2025-04-02 21:47:47 +1030

    Another image update

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,8 +76,8 @@ export default defineConfig({
 
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+
+    retries: 0, // we want to be stable, not retrying
 
     /* Opt out of parallel tests on CI. */
     // TBD UNCOMMENT
