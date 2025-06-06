fafb7aa4f38b227ca61b840ca165e4e0496774e0
Akita Noek <anoek@online-go.com>
2025-06-06 08:33:45 -0600

    Admin page stubs

--- src/views/Admin/index.ts
+++ src/views/Admin/index.ts
@@ -15,7 +15,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export * from "./Admin";
-export * from "./MerchantLog";
-export * from "./FlaggedGames";
-export * from "./OnlineLeaguesAdmin";
+export { Admin, MerchantLog, FlaggedGames, OnlineLeaguesAdmin } from "@moderator-ui/Admin";
