08978a398aec69870175001b090524a50a27b088
GreenAsJade <github@gregories.net>
2025-06-24 23:14:43 +0930

    fix props

--- src/stubs/moderator-ui/ModTools/ModLog.tsx
+++ src/stubs/moderator-ui/ModTools/ModLog.tsx
@@ -5,7 +5,10 @@
 
 import * as React from "react";
 
-export type ModLogProps = {};
+export type ModLogProps = {
+    user_id: number;
+    groomData?: (data: rest_api.moderation.ModLogEntry[]) => rest_api.moderation.ModLogEntry[];
+};
 
 export function ModLog(_props: ModLogProps): React.ReactElement {
     return <div>Not implemented</div>;
--- src/stubs/moderator-ui/ModTools/ModTools.tsx
+++ src/stubs/moderator-ui/ModTools/ModTools.tsx
@@ -5,7 +5,11 @@
 
 import * as React from "react";
 
-export type ModToolsProps = {};
+export type ModToolsProps = {
+    user_id: number;
+    show_mod_log: boolean;
+    collapse_same_users?: boolean;
+};
 
 export function ModTools(_props: ModToolsProps): React.ReactElement {
     return <div>Not implemented</div>;
