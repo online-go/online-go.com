eaa9c25a0acc72fcfd6dc6ba8ce3dd93e20e0076
GreenAsJade <github@gregories.net>
2025-06-24 23:02:31 +0930

    Moved mod tools into moderator-ui

---
+++ src/stubs/moderator-ui/ModTools/ModLog.tsx
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C)  Online-Go.com
+ *
+ */
+
+import * as React from "react";
+
+export type ModLogProps = {};
+
+export function ModLog(_props: ModLogProps): React.ReactElement {
+    return <div>Not implemented</div>;
+}
---
+++ src/stubs/moderator-ui/ModTools/ModTools.tsx
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C)  Online-Go.com
+ *
+ */
+
+import * as React from "react";
+
+export type ModToolsProps = {};
+
+export function ModTools(_props: ModToolsProps): React.ReactElement {
+    return <div>Not implemented</div>;
+}
