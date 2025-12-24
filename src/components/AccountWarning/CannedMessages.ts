8e79aec80b9a8dd0e23c9f55c3252f3501323c67
GreenAsJade <github@gregories.net>
2025-12-24 12:04:48 +1030

    Make the message slightly less passive

--- src/components/AccountWarning/CannedMessages.ts
+++ src/components/AccountWarning/CannedMessages.ts
@@ -128,7 +128,7 @@ We've noticed that you joined game #{{game_id}} but didn't make any moves.
 
 It's possible you didn't notice this game start - we understand that.
 
-If you can be alert for this, so that other users are not left waiting and wondering, that would be great..
+Please avoid this if possible, so that other users are not left waiting and wondering.
 
 Thank you for helping keep OGS enjoyable for everyone. We appreciate it.
 `,
